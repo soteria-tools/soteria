76456cec528128d76e4278869412d9f3e85332d3
Sacha Ayoun <sachaayoun@gmail.com>
2024-10-17 21:50:52 +0200

    globalStorage, not storage
    Signed-off-by: Sacha Ayoun <sachaayoun@gmail.com>

--- extension/bfa_vscode.ml
+++ extension/bfa_vscode.ml
@@ -1,11 +1,19 @@
-open Vscode
-open Promise.Syntax
+open Import
+
+let storage_path instance extension =
+  let uri = ExtensionContext.globalStorageUri extension in
+  let path = Uri.fsPath uri in
+  Server.log_message ~instance `Info "Got storage path: %s" path;
+  path
 
 let activate (extension : Vscode.ExtensionContext.t) =
   let instance = Server.empty_instance () in
-  let+ () = Server.start instance in
+  Server.log_message ~instance `Info "About to get storage path";
+  let* _ = Install.check_version (storage_path instance extension) in
+  let* () = Server.start instance in
   ExtensionContext.subscribe ~disposable:(Server.disposable instance) extension;
-  Bfa_commands.register_all_commands extension instance
+  Bfa_commands.register_all_commands extension instance;
+  Promise.return ()
 
 let () =
   let open Js_of_ocaml.Js in
--- extension/install.ml
+++ extension/install.ml
@@ -13,3 +13,32 @@ let url =
         in
         computed := Some url;
         url
+
+let check_version storage_path =
+  let dyld_lib_path =
+    let name = "DYLD_LIBRARY_PATH" in
+    let new_path = Filename.concat storage_path "/lib" in
+    let path =
+      match Node.Process.Env.get name with
+      | None -> new_path
+      | Some old_path -> new_path ^ ":" ^ old_path
+    in
+    (name, path)
+  in
+  let z3_path =
+    let name = "BFA_Z3_PATH" in
+    let path = storage_path ^ "/bin/z3" in
+    (name, path)
+  in
+  let env = Interop.Dict.of_list [ dyld_lib_path; z3_path ] in
+  let options = Node.ChildProcess.Options.create ~env () in
+  let command = Filename.concat storage_path "/bin/bfa-c" in
+  let args = [| "lsp"; "--version" |] in
+  let+ Node.ChildProcess.{ stdout; exitCode; _ } =
+    Node.ChildProcess.spawn ~options command args
+  in
+  let () =
+    Import.show_message `Info "Version of installed bfa-c: %s (exit status: %d)"
+      stdout exitCode
+  in
+  exitCode = 0
