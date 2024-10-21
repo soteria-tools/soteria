c95f2a521080f5147d99df287f7ba3194580e876
Sacha Ayoun <sachaayoun@gmail.com>
2024-10-21 14:28:25 +0100

    fetch script re-uploads to ssh
    Signed-off-by: Sacha Ayoun <sachaayoun@gmail.com>

--- github_api_fetch.sh
+++ github_api_fetch.sh
@@ -2,28 +2,39 @@
 ### This script is for when I want to auto-fetch the latest version from the GitHub API.
 ###
 
+set -ex
+
 # Variables
-REPO="org/repo"
-ARTIFACT_NAME="name"
-TOKEN="YOUR_GITHUB_TOKEN"
+REPO="bugs-for-all/bfa-ocaml"
+source secrets.env
+ARTIFACTS=("macos-latest-package" "macos-13-package")
 
 # Get the latest workflow run
 LATEST_RUN_ID=$(curl -s -H "Authorization: token $TOKEN" \
   https://api.github.com/repos/$REPO/actions/runs?per_page=1 \
   | jq -r '.workflow_runs[0].id')
+  
+echo "LATEST_RUN_ID: $LATEST_RUN_ID"
 
-# Get the artifact ID for the artifact with the specific name
-ARTIFACT_ID=$(curl -s -H "Authorization: token $TOKEN" \
-  https://api.github.com/repos/$REPO/actions/runs/$LATEST_RUN_ID/artifacts \
-  | jq -r ".artifacts[] | select(.name==\"$ARTIFACT_NAME\") | .id")
+for ARTIFACT_NAME in "${ARTIFACTS[@]}"; do
+  echo "Processing artifact: $ARTIFACT_NAME"
 
-# Download the artifact
-curl -s -H "Authorization: token $TOKEN" \
-  -L https://api.github.com/repos/$REPO/actions/artifacts/$ARTIFACT_ID/zip \
-  -o artifact.zip
+  # Get the artifact ID for the artifact with the specific name
+  ARTIFACT_ID=$(curl -s -H "Authorization: token $TOKEN" \
+    https://api.github.com/repos/$REPO/actions/runs/$LATEST_RUN_ID/artifacts \
+    | jq -r ".artifacts[] | select(.name==\"$ARTIFACT_NAME\") | .id")
 
-# Compute the SHA256 hash of the artifact
-SHA256=$(shasum -a 256 artifact.zip | awk '{print $1}')
+  echo "ARTIFACT_ID: $ARTIFACT_ID"
 
-# Output the SHA256
-echo "SHA256: $SHA256"
+  # Download the artifact
+  curl -s -H "Authorization: token $TOKEN" \
+    -L https://api.github.com/repos/$REPO/actions/artifacts/$ARTIFACT_ID/zip \
+    -o $ARTIFACT_NAME.zip
+
+  scp $ARTIFACT_NAME.zip $SSH_USER@$SSH_HOST:$SSH_PATH
+  
+  rm $ARTIFACT_NAME.zip
+  
+  echo "Pushed $ARTIFACT_NAME.zip"
+  
+done
