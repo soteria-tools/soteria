119103bc5c6934284bfadd9c20b56472305cc5ae
N1ark <opale.sjostedt@gmail.com>
2025-03-23 13:54:53 +0000

    Add license to Kani lib files

--- bfa_rust/kani_lib/kani/src/lib.rs
+++ bfa_rust/kani_lib/kani/src/lib.rs
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
 #![feature(register_tool)]
 #![register_tool(kanitool)]
 
--- bfa_rust/kani_lib/kani_macros/src/lib.rs
+++ bfa_rust/kani_lib/kani_macros/src/lib.rs
@@ -1,3 +1,5 @@
+// Copyright Kani Contributors
+// SPDX-License-Identifier: Apache-2.0 OR MIT
 #![feature(proc_macro_diagnostic)]
 #![feature(proc_macro_span)]
 mod derive;
