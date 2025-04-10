a12c9ea7a0a2b3c82f9de6184182559ee5358d39
Sacha Ayoun <sachaayoun@gmail.com>
2025-04-10 18:29:49 +0100

    Hotfix excl missing errors which had the wrong type (#17)
    * hotfix
    
    Signed-off-by: Sacha-Élie Ayoun <sachaayoun@gmail.com>
    
    * more
    
    ---------
    
    Signed-off-by: Sacha-Élie Ayoun <sachaayoun@gmail.com>

--- bfa/lib/symex/excl.ml
+++ bfa/lib/symex/excl.ml
@@ -7,12 +7,12 @@ module Make (Symex : Symex.S) = struct
   let load (st : 'a t option) =
     match st with
     | Some x -> Symex.Result.ok (x, st)
-    | None -> Symex.Result.miss ()
+    | None -> Symex.Result.miss []
 
   let store x (st : 'a t option) =
     match st with
     | Some _ -> Symex.Result.ok ((), Some x)
-    | None -> Symex.Result.miss ()
+    | None -> Symex.Result.miss []
 
   let serialize serialize_val x = serialize_val x
   let pp_serialized = pp
@@ -23,13 +23,14 @@ module Make (Symex : Symex.S) = struct
 
   let subst_serialized subst_inner subst_var x = subst_inner subst_var x
 
-  let consume ~sem_eq (serialized : 'a serialized) (t : 'a t option) =
+  let consume ~sem_eq (serialized : 'a serialized) (t : 'a t option) :
+      ('a t option, 'err, 'a serialized list) Symex.Result.t =
     let open Symex.Syntax in
     match t with
     | Some x ->
         let+ () = Symex.assume [ sem_eq x serialized ] in
         Compo_res.Ok None
-    | None -> Symex.Result.miss serialized
+    | None -> Symex.Result.miss [ serialized ]
 
   let produce (serialized : 'a serialized) (t : 'a t option) =
     match t with
