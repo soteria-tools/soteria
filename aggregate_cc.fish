bccd8ab4957b7c7a5d3aacb67f842343d90ebedd
Sacha-Élie Ayoun <sachaayoun@gmail.com>
2025-04-16 18:12:05 +0100

    Handle multiple files when generating summaries
    Signed-off-by: Sacha-Élie Ayoun <sachaayoun@gmail.com>

--- aggregate_cc.fish
+++ aggregate_cc.fish
@@ -2,20 +2,30 @@
 
 set err_counter 0
 
+
+
 rm -rf json_dump
 mkdir -p json_dump
-for file in ../Collections-C/src/*.c
-  echo (basename $file .c)
-  _build/install/default/bin/soteria-c gen-summaries $file -I ../Collections-C/src/include/ --dump-unsupported json_dump/(basename $file .c).json || set err_counter (math $err_counter + 1)
-end
+opam exec -- dune exec -- soteria-c gen-summaries \
+       ../Collections-C/src/cc_array.c ../Collections-C/src/cc_common.c \
+       ../Collections-C/src/cc_deque.c ../Collections-C/src/cc_hashset.c \
+       ../Collections-C/src/cc_list.c \
+       # ../Collections-C/src/cc_hashtable.c \ # TODO: Cerberus doesn't parse this correctly
+       ../Collections-C/src/cc_pqueue.c ../Collections-C/src/cc_queue.c \
+       ../Collections-C/src/cc_ring_buffer.c ../Collections-C/src/cc_slist.c \
+       ../Collections-C/src/cc_stack.c ../Collections-C/src/cc_treeset.c \
+       ../Collections-C/src/cc_treetable.c ../Collections-C/src/cc_tsttable.c \
+       -I ../Collections-C/src/include/ \
+       --dump-unsupported json_dump/dump.json \
+       || set err_counter (math $err_counter + 1)
 
 
-echo "Generating json"
 
-jq -s 'reduce .[] as $obj ({};
-  reduce ($obj | keys_unsorted[]) as $key (.;
-    .[$key] = (.[$key] // 0) + $obj[$key]
-    )
-  ) | to_entries | sort_by(.value) | reverse | from_entries' json_dump/*.json > out.json
 
-echo "Done, $err_counter files failed"
+# jq -s 'reduce .[] as $obj ({};
+#   reduce ($obj | keys_unsorted[]) as $key (.;
+#     .[$key] = (.[$key] // 0) + $obj[$key]
+#     )
+#   ) | to_entries | sort_by(.value) | reverse | from_entries' json_dump/*.json > out.json
+
+# echo "Done, $err_counter files failed"
