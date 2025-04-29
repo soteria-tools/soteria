5cd642cb490376ea5123e097c758189af170178d
N1ark <opale.sjostedt@gmail.com>
2025-04-29 14:03:05 +0100

    Format JS + add filtering

--- soteria/lib/logging/assets/soteria_logs.js
+++ soteria/lib/logging/assets/soteria_logs.js
@@ -40,72 +40,117 @@ function flattenDirectDetails(element) {
 }
 
 function observeSearch() {
-  const searchInput = document.querySelector('.search-input');
-  const logMessages = document.querySelectorAll('div.log-msg');
+  const searchInput = document.getElementById("search-input");
+  const logMessages = document.querySelectorAll("div.log-msg");
 
-  searchInput.addEventListener('input', () => {
-    const searchRe = new RegExp(searchInput.value, 'i');
+  searchInput.addEventListener("input", () => {
+    const searchRe = new RegExp(searchInput.value, "i");
 
-    logMessages.forEach(message => {
+    logMessages.forEach((message) => {
       const messageText = message.childNodes[0].textContent;
-      message.style.display = searchRe.test(messageText) ? 'block' : 'none';
+      message.style.display = searchRe.test(messageText) ? "block" : "none";
     });
   });
 }
 
 function keybindings() {
-  const searchInput = document.querySelector('.search-input');
+  const searchInput = document.getElementById("search-input");
 
-  document.addEventListener('keydown', event => {
+  document.addEventListener("keydown", (event) => {
     if (event.target === searchInput) return;
+    if (event.ctrlKey || event.metaKey) return;
 
-    const details = document.querySelectorAll('details');
+    const details = document.querySelectorAll("details");
     console.log(event.key);
-    if (event.key === 'Escape') {
-      searchInput.value = '';
-      searchInput.dispatchEvent(new Event('input'));
+    if (event.key === "Escape") {
+      searchInput.value = "";
+      searchInput.dispatchEvent(new Event("input"));
       searchInput.focus();
       event.preventDefault();
       event.stopPropagation();
-    } else if (event.key === 'o') {
-      details.forEach(detail => {
+    } else if (event.key === "o") {
+      details.forEach((detail) => {
         detail.open = true;
       });
-    } else if (event.key === 'c') {
-      details.forEach(detail => {
+    } else if (event.key === "c") {
+      details.forEach((detail) => {
         detail.open = false;
       });
-    } else if (event.key === 'ArrowRight') {
+    } else if (event.key === "ArrowRight") {
       let active = document.activeElement;
-      if (active.tagName === 'SUMMARY') {
+      if (active.tagName === "SUMMARY") {
         active = active.parentNode;
       }
       if (active.open) {
-        active
-          .querySelectorAll('details')
-          .forEach(detail => { detail.open = true; });
+        active.querySelectorAll("details").forEach((detail) => {
+          detail.open = true;
+        });
       } else {
         active.open = true;
       }
-    } else if (event.key === 'ArrowLeft') {
+    } else if (event.key === "ArrowLeft") {
       let active = document.activeElement;
-      if (active.tagName === 'SUMMARY') {
+      if (active.tagName === "SUMMARY") {
         active = active.parentNode;
       }
-      active
-        .querySelectorAll('details')
-        .forEach(detail => { if (detail !== active) { detail.open = false; } });
+      active.querySelectorAll("details").forEach((detail) => {
+        if (detail !== active) {
+          detail.open = false;
+        }
+      });
       active.open = false;
     }
   });
 }
 
+function populateFilter() {
+  const filterBox = document.getElementById("filters");
+  const filterClasses = Array.from(document.querySelectorAll(".log-msg"))
+    .map((msg) => msg.className.split(" "))
+    .flat();
+  const uniqueClasses = [...new Set(filterClasses)];
+  const prio = {
+    SMT: 0,
+    TRACE: 1,
+    DEBUG: 2,
+    INFO: 3,
+    WARN: 4,
+    ERROR: 5,
+    APP: 6,
+  };
+  uniqueClasses.sort((l, r) => prio[r] - prio[l]);
+  uniqueClasses.forEach((className) => {
+    if (className === "log-msg") return;
+    const option = document.createElement("input");
+    option.type = "checkbox";
+    option.name = className;
+    option.id = className;
+    option.checked = true;
+    filterBox.appendChild(option);
+
+    const label = document.createElement("label");
+    label.htmlFor = className;
+    label.textContent = className;
+    label.className = className;
+    filterBox.appendChild(label);
+
+    option.addEventListener("change", () => {
+      const checked = option.checked;
+      const messages = document.querySelectorAll(`.log-msg.${className}`);
+      messages.forEach((msg) => {
+        msg.style.display = checked ? "block" : "none";
+      });
+    });
+  });
+}
+
 // Start from the body
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   // This is now useless since we show all branches as sections
   // even those which are not feasible.
   // Let's keep the code around for now, in case we want to change this.
   // flattenDirectDetails(document.body);
   observeSearch();
   keybindings();
+  populateFilter();
 });
