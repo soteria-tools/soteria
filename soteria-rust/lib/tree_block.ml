9126d29371a6d8e3393ada12d4b43c0c75975e3c
N1ark <opale.sjostedt@gmail.com>
2025-05-26 09:25:24 -0700

    Factor out none+bound check

--- soteria-rust/lib/tree_block.ml
+++ soteria-rust/lib/tree_block.ml
@@ -715,7 +715,7 @@ let with_bound_check (t : t) (ofs : [< T.sint ] Typed.t) f =
           Result.error `OutOfBounds)
         else Result.ok ()
   in
-  let++ res, root = f () in
+  let++ res, root = f t.root in
   (res, { t with root })
 
 let of_opt ?(mk_fixes = fun () -> Rustsymex.return []) = function
@@ -723,6 +723,11 @@ let of_opt ?(mk_fixes = fun () -> Rustsymex.return []) = function
       let+ fixes = mk_fixes () in
       Missing fixes
   | Some t -> Result.ok t
+
+let with_bound_and_owned_check ?mk_fixes t ofs f =
+  let** t = of_opt ?mk_fixes t in
+  let++ res, root = with_bound_check t ofs f in
+  (res, Some root)
 
 let to_opt t = if is_empty t then None else Some t
 
@@ -743,99 +748,55 @@ let load ~is_move ~ignore_borrow ofs ty tag tb t =
   let* size = Layout.size_of_s ty in
   let** t = of_opt ~mk_fixes:(mk_fix_typed ofs ty) t in
   let++ res, tree =
-    let@ () = with_bound_check t (ofs +@ size) in
-    Tree.load ~is_move ~ignore_borrow ofs size ty tag tb t.root
+    let@ t = with_bound_check t (ofs +@ size) in
+    Tree.load ~is_move ~ignore_borrow ofs size ty tag tb t
   in
   (res, to_opt tree)
 
 let store ofs ty sval tag tb t =
-  match t with
-  | None -> miss_no_fix ~msg:"outer store" ()
-  | Some t ->
-      let* size = Layout.size_of_s ty in
-      let++ (), tree =
-        let@ () = with_bound_check t (ofs +@ size) in
-        Tree.store ofs size ty sval tag tb t.root
-      in
-      ((), to_opt tree)
+  let* size = Layout.size_of_s ty in
+  let@ t = with_bound_and_owned_check t (ofs +@ size) in
+  Tree.store ofs size ty sval tag tb t
 
 let get_raw_tree_owned ofs size t =
-  let** t = of_opt t in
-  let++ res, tree =
-    let@ () = with_bound_check t (ofs +@ size) in
-    let** tree, t = Tree.get_raw ofs size t.root in
-    if Node.is_fully_owned tree.node then
-      let tree = Tree.offset ~by:~-ofs tree in
-      Result.ok (tree, t)
-    else miss_no_fix ~msg:"get_raw_tree_owned" ()
-  in
-  (res, to_opt tree)
+  let@ t = with_bound_and_owned_check t (ofs +@ size) in
+  let** tree, t = Tree.get_raw ofs size t in
+  if Node.is_fully_owned tree.node then
+    let tree = Tree.offset ~by:~-ofs tree in
+    Result.ok (tree, t)
+  else miss_no_fix ~msg:"get_raw_tree_owned" ()
 
 (* This is used for copy_nonoverapping.
    It is an action on the destination block, and assumes the received tree is at offset 0 *)
 let put_raw_tree ofs (tree : Tree.t) t :
     (unit * t option, 'err, 'fix list) Result.t =
-  let** t = of_opt t in
   let size = Range.size tree.range in
+  let@ t = with_bound_and_owned_check t (ofs +@ size) in
   let tree = Tree.offset ~by:ofs tree in
-  let++ res, t =
-    let@ () = with_bound_check t (ofs +@ size) in
-    Tree.put_raw tree t.root
-  in
-  (res, to_opt t)
+  Tree.put_raw tree t
 
 let alloc size =
   { root = Tree.uninit Tree_borrow.empty_state (0s, size); bound = Some size }
 
 let uninit_range ofs size t =
-  match t with
-  | None -> miss_no_fix ~msg:"uninit on none" ()
-  | Some t ->
-      let++ (), tree =
-        let@ () = with_bound_check t (ofs +@ size) in
-        Tree.uninit_range ofs size t.root
-      in
-      ((), to_opt tree)
+  let@ t = with_bound_and_owned_check t (ofs +@ size) in
+  Tree.uninit_range ofs size t
 
 let zero_range ofs size t =
-  match t with
-  | None -> miss_no_fix ~msg:"zero on none" ()
-  | Some t ->
-      let++ (), tree =
-        let@ () = with_bound_check t (ofs +@ size) in
-        Tree.zero_range ofs size t.root
-      in
-      ((), to_opt tree)
+  let@ t = with_bound_and_owned_check t (ofs +@ size) in
+  Tree.zero_range ofs size t
 
 let protect ofs size tag tb t =
-  match t with
-  | None -> miss_no_fix ~msg:"protect on none" ()
-  | Some t ->
-      let++ (), tree =
-        let@ () = with_bound_check t (ofs +@ size) in
-        Tree.protect ofs size tag tb t.root
-      in
-      ((), to_opt tree)
+  let@ t = with_bound_and_owned_check t (ofs +@ size) in
+  Tree.protect ofs size tag tb t
 
 let unprotect ofs size tag tb t =
-  match t with
-  | None -> miss_no_fix ~msg:"protect on none" ()
-  | Some t ->
-      let++ (), tree =
-        let@ () = with_bound_check t (ofs +@ size) in
-        Tree.unprotect ofs size tag tb t.root
-      in
-      ((), to_opt tree)
+  let@ t = with_bound_and_owned_check t (ofs +@ size) in
+  Tree.unprotect ofs size tag tb t
 
 let tb_access ofs size tag tb t =
-  match t with
-  | None -> miss_no_fix ~msg:"protect on none" ()
-  | Some t ->
-      let++ (), tree =
-        let@ () = with_bound_check t (ofs +@ size) in
-        Tree.tb_access ofs size tag tb t.root
-      in
-      ((), to_opt tree)
+  let@ t = with_bound_and_owned_check t (ofs +@ size) in
+  Tree.tb_access ofs size tag tb t
 
 (** Logic *)
 
