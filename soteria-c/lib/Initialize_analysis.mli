e9b888d128956256f74ffacf9175e49adc291ad3
Sacha-Élie Ayoun <sachaayoun@gmail.com>
2025-06-13 06:51:52 +0100

    effectfull accesses, remove global init
    Signed-off-by: Sacha-Élie Ayoun <sachaayoun@gmail.com>

--- soteria-c/lib/Initialize_analysis.mli
+++
@@ -1,4 +0,0 @@
-val init_once : unit -> unit
-val register_once_initialiser : (unit -> unit) -> unit
-val reinit : Ail_tys.sigma -> unit
-val register_before_each_initialiser : (Ail_tys.sigma -> unit) -> unit
