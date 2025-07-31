6d9f9a74ed61347d9c499e9a26fe443a873a9e9f
Sacha Ayoun <sachaayoun@gmail.com>
2025-07-31 18:40:15 +0200

    initial commit on documentation and tutorial
    Signed-off-by: Sacha Ayoun <sachaayoun@gmail.com>

---
+++ soteria/lib/soteria.ml
@@ -0,0 +1,7 @@
+(** Comprehensive library for symbolic execution in OCaml. *)
+
+module Soteria_logs = Soteria_logs
+module Soteria_terminal = Soteria_terminal
+module Soteria_c_values = Soteria_c_values
+module Soteria_symex = Soteria_symex
+module Soteria_std = Soteria_std
