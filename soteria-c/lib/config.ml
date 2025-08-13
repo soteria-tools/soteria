c3786eadfcf5deac9509fee108754d709727b976
Sacha Ayoun <sachaayoun@gmail.com>
2025-08-13 11:18:07 +0100

    fix
    Signed-off-by: Sacha Ayoun <sachaayoun@gmail.com>

--- soteria-c/lib/config.ml
+++ soteria-c/lib/config.ml
@@ -6,59 +6,64 @@ type t = {
         | [ x ] -> x
         | _ -> failwith "Multiple auto-include paths found"]
       [@docv "PATH"]
-      [@names [ "auto-include-path" ] [@env "SOTERIA_AUTO_INCLUDE_PATH"]]
+      [@names [ "auto-include-path" ]]
+      [@env "SOTERIA_AUTO_INCLUDE_PATH"]
       (** Path to the directory that contains the soteria-c.h *)
   dump_unsupported_file : string option;
       [@docv "FILE"]
-      [@names [ "dump-unsupported" ] [@env "SOTERIA_DUMP_UNSUPPORTED"]]
+      [@names [ "dump-unsupported" ]]
+      [@env "SOTERIA_DUMP_UNSUPPORTED"]
       (** Dump a json file with unsupported features and their number of
           occurences *)
   no_ignore_parse_failures : bool;
       [@make.default false]
-      [@names
-        [ "no-ignore-parse-failures" ] [@env "SOTERIA_NO_IGNORE_PARSE_FAILURES"]]
+      [@names [ "no-ignore-parse-failures" ]]
+      [@env "SOTERIA_NO_IGNORE_PARSE_FAILURES"]
       (** Files that cannot be parsed correctly are ignored by default, this
           flag deactivates that behaviour. *)
   no_ignore_duplicate_symbols : bool;
       [@make.default false]
-      [@names
-        [ "no-ignore-duplicate-symbols" ]
-        [@env "SOTERIA_NO_IGNORE_DUPLICATE_SYMBOLS"]]
+      [@names [ "no-ignore-duplicate-symbols" ]]
+      [@env "SOTERIA_NO_IGNORE_DUPLICATE_SYMBOLS"]
       (** Programs that contain duplicate symbols are ignored by default, this
           flag deactivates that behaviour. *)
   parse_only : bool;
       [@make.default false]
-      [@names [ "parse-only" ] [@env "SOTERIA_PARSE_ONLY"]]
+      [@names [ "parse-only" ]]
+      [@env "SOTERIA_PARSE_ONLY"]
       (** Only parse and link the C program, do not perform analysis *)
   dump_summaries_file : string option;
       [@docv "FILE"]
-      [@names
-        [ "dump-summaries"; "dump-summaries-to" ]
-        [@env "SOTERIA_DUMP_SUMMARIES_FILE"]]
+      [@names [ "dump-summaries"; "dump-summaries-to" ]]
+      [@env "SOTERIA_DUMP_SUMMARIES_FILE"]
       (** Dump the generated summaries to a file *)
   show_manifest_summaries : bool;
       [@make.default false]
-      [@names
-        [ "show-manifest-summaries" ] [@env "SOTERIA_SHOW_MANIFEST_SUMMARIES"]]
+      [@names [ "show-manifest-summaries" ]]
+      [@env "SOTERIA_SHOW_MANIFEST_SUMMARIES"]
       (** Print a corresponding manifest summary after the bug report if a bug
           is found *)
   alloc_cannot_fail : bool;
       [@make.default false]
-      [@names [ "alloc-cannot-fail" ] [@env "SOTERIA_ALLOC_CANNOT_FAIL"]]
+      [@names [ "alloc-cannot-fail" ]]
+      [@env "SOTERIA_ALLOC_CANNOT_FAIL"]
       (** Assume allocations cannot fail *)
   use_cerb_headers : bool;
       [@make.default false]
-      [@names [ "use-cerb-headers" ] [@env "SOTERIA_USE_CERB_HEADERS"]]
+      [@names [ "use-cerb-headers" ]]
+      [@env "SOTERIA_USE_CERB_HEADERS"]
       (** Use the Cerberus-provided standard headers instead of the system
           headers. *)
   infinite_fuel : bool;
       [@make.default false]
-      [@names [ "infinite-fuel" ] [@env "SOTERIA_INFINITE_FUEL"]]
+      [@names [ "infinite-fuel" ]]
+      [@env "SOTERIA_INFINITE_FUEL"]
       (** Infinite fuel for the analysis. If there is an unbounded loop, the
           analysis will not stop. (Used only for whole-program analysis) *)
   cbmc_compat : bool;
       [@make.default false]
-      [@names [ "cbmc"; "cbmc-compat" ] [@env "SOTERIA_CBMC_COMPAT"]]
+      [@names [ "cbmc"; "cbmc-compat" ]]
+      [@env "SOTERIA_CBMC_COMPAT"]
       (** Enable support for a subset of the __CPROVER_ API. *)
   havoc_undefined_funs : bool;
       [@make.default false]
