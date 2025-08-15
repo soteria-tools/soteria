ccabbfde0195c962a9de323034efa1a2f651010b
pcarrott <pedro.carrott.99@gmail.com>
2025-08-15 15:05:50 +0100

    rename monomorphize flag

--- soteria-rust/lib/config.ml
+++ soteria-rust/lib/config.ml
@@ -9,8 +9,10 @@ type t = {
       (** Ignore memory leaks *)
   ignore_aliasing : bool; [@make.default false] [@names [ "ignore-aliasing" ]]
       (** Ignore pointer aliasing rules (tree borrows) *)
-  monomorphize : bool; [@make.default false] [@names [ "monomorphize" ]]
-      (** Full monomorphization, will crash on [dyn Trait] *)
+  monomorphize : bool;
+      [@make.default false] [@names [ "monomorphize-experimental" ]]
+      (** Use Charon's new monomorphization, which may cause unexpected results
+          but resolves drops. *)
   with_kani : bool; [@make.default false] [@names [ "kani" ]]
       (** Use the Kani library *)
   with_miri : bool; [@make.default false] [@names [ "miri" ]]
