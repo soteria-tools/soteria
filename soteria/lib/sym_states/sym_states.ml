0862a8a858387959a92ef4e917bcd2bfe85f07a1
Sacha Ayoun <sachaayoun@gmail.com>
2025-09-05 18:19:54 +0100

    doc

---
+++ soteria/lib/sym_states/sym_states.ml
@@ -0,0 +1,11 @@
+(** A collection of state monad transformers for building compositional symbolic
+    execution states. *)
+
+module Bi_abd = Bi_abd
+module Excl = Excl
+module Freeable = Freeable
+module Plist = Plist
+module Pmap = Pmap
+module Pure_fun = Pure_fun
+module Tree_block = Tree_block
+module With_info = With_info
