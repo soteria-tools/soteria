4894355e4fad7510ddcce039760694c0a4ee8175
Sacha Ayoun <sachaayoun@gmail.com>
2025-09-12 16:39:19 +0100

    add if%sure to ppx (and simplify ppx)
    Signed-off-by: Sacha Ayoun <sachaayoun@gmail.com>

--- soteria/ppx/ppx_symex.ml
+++ soteria/ppx/ppx_symex.ml
@@ -1,7 +1,7 @@
 open Ppxlib
 
 let ext ext =
-  let open Ppx_symex_expander.If_sat in
+  let open Expander.If_sat in
   Extension.declare_with_path_arg
     (Extension_name.to_string ext)
     Extension.Context.expression
@@ -10,17 +10,17 @@ let ext ext =
 
 (* Registring [if%sat] *)
 let () =
-  let open Ppx_symex_expander.If_sat in
-  Driver.register_transformation
-    (Extension_name.to_string Sat)
-    ~extensions:[ ext Sat ];
-  Driver.register_transformation
-    (Extension_name.to_string Sat1)
-    ~extensions:[ ext Sat1 ]
+  let open Expander.If_sat in
+  let register extension =
+    Driver.register_transformation
+      (Extension_name.to_string extension)
+      ~extensions:[ ext extension ]
+  in
+  List.iter register [ Sat; Sat1 ]
 
 (* Register [0s], [1s] etc. *)
 let () =
-  let open Ppx_symex_expander.Sym_constants in
+  let open Expander.Sym_constants in
   let kind = Context_free.Rule.Constant_kind.Integer in
   let rule = Context_free.Rule.constant kind suffix rewriter in
   Driver.register_transformation ~rules:[ rule ] "sym_constants"
