fd68cf4565027633cc7d87913b7af852523748a6
Sacha Ayoun <sachaayoun@gmail.com>
2025-09-14 19:56:27 +0100

    move cram tests
    Signed-off-by: Sacha Ayoun <sachaayoun@gmail.com>

--- soteria/tests/symex/deep_give_up.ml
+++ soteria/tests/symex/deep_give_up.ml
@@ -1,4 +1,8 @@
-open Csymex
+open
+  Soteria.Symex.Make
+    (Soteria.Symex.Meta.Dummy)
+    (Soteria.C_values.C_solver.Z3_solver)
+
 open Syntax
 open Soteria.C_values
 
@@ -15,18 +19,16 @@ let complex_process =
   let* y = nondet Typed.t_int in
   if%sat x >@ y then
     if%sat x >@ Typed.zero then Result.ok (x +@ y) else Result.ok (x -@ y)
-  else
-    if%sat x ==@ y then give_up ~loc:Cerb_location.unknown "x == y"
-    else Result.error "okkk"
+  else if%sat x ==@ y then give_up ~loc:() "x == y" else Result.error "okkk"
 
 let _give_up_in_ux =
-  let results = Csymex.run ~mode:UX complex_process in
+  let results = run ~mode:UX complex_process in
   Fmt.pr "@[<v 2>Csymex.run ~mode:UX complex_process:@ %a@]@\n@\n"
     (Fmt.Dump.list (pp_branch Fmt.string))
     results
 
 let _give_up_in_ux_res =
-  let results = Csymex.Result.run ~mode:UX complex_process in
+  let results = Result.run ~mode:UX complex_process in
   Fmt.pr "@[<v 2>Csymex.Result.run ~mode:UX complex_process:@ %a@]@\n@\n"
     (Fmt.Dump.list (pp_branch (Soteria.Symex.Or_gave_up.pp Fmt.string)))
     results
@@ -34,7 +36,7 @@ let _give_up_in_ux_res =
 let _give_up_in_ox_exn =
   Fmt.pr "Csymex.run ~mode:OX complex_process;; EXPECTING EXCEPTION -- ";
   try
-    let results = Csymex.run ~mode:OX complex_process in
+    let results = run ~mode:OX complex_process in
     Fmt.pr "In OX with Csymex.run: %a@\n"
       (Fmt.Dump.list (pp_branch Fmt.string))
       results
@@ -42,7 +44,7 @@ let _give_up_in_ox_exn =
     Fmt.pr "Caught Gave_up in OX: %s@\n@\n" reason
 
 let _give_up_in_ox_res =
-  let results = Csymex.Result.run ~mode:OX complex_process in
+  let results = Result.run ~mode:OX complex_process in
   Fmt.pr "@[<v 2>Csymex.Result.run ~mode:OX complex_process:@ %a@]@\n@\n"
     (Fmt.Dump.list (pp_branch (Soteria.Symex.Or_gave_up.pp Fmt.string)))
     results
