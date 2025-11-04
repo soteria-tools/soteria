d39d7eb06e527cdba80812951ad1b52f7c8dcf82
N1ark <opale.sjostedt@gmail.com>
2025-11-04 09:56:14 +0000

    Update soteria/lib/bv_values/analyses.ml
    Co-authored-by: Sacha Ayoun <sachaayoun@gmail.com>

--- soteria/lib/bv_values/analyses.ml
+++ soteria/lib/bv_values/analyses.ml
@@ -62,6 +62,10 @@ module None : S = struct
 end
 
 module Interval : S = struct
+  (** An interval analysis for bitvectors inspired by
+       https://ceur-ws.org/Vol-1617/paper8.pdf *)
+
+
   (* we only include stuff from Z we want  *)
   open struct
     let one = Z.one
