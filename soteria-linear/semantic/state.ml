2aa2bb124efb322bd9095fad9994c194d1d05074
Sacha Ayoun <sachaayoun@gmail.com>
2025-11-23 17:49:28 +0000

    simplify interface
    Signed-off-by: Sacha Ayoun <sachaayoun@gmail.com>

--- soteria-linear/semantic/state.ml
+++ soteria-linear/semantic/state.ml
@@ -4,21 +4,21 @@ type err = [ `UseAfterFree | `Interp of string ]
 [@@deriving show { with_path = false }]
 
 module PMap = Soteria.Sym_states.Pmap.Make (Symex) (S_int)
-module Excl = Soteria.Sym_states.Excl.Make (Symex)
+module Excl_val = Soteria.Sym_states.Excl.Make (Symex) (S_val)
+module Freeable = Soteria.Sym_states.Freeable.Make (Symex)
 
-type t = S_val.T.any S_val.t Excl.t PMap.t option
-[@@deriving show { with_path = false }]
+type t = Excl_val.t PMap.t option [@@deriving show { with_path = false }]
 
-type fixes = S_val.T.any S_val.t Excl.serialized PMap.serialized
+type fixes = Excl_val.serialized PMap.serialized
 [@@deriving show { with_path = false }]
 
 let empty : t = None
-let load addr st = PMap.wrap Excl.load addr st
-let store addr value st = PMap.wrap (Excl.store value) addr st
+let load addr st = PMap.wrap Excl_val.load addr st
+let store addr value st = PMap.wrap (Excl_val.store value) addr st
 
 let alloc st =
-  let v_false = (S_val.v_false :> S_val.T.any S_val.t) in
+  let v_false = (S_val.v_false :> S_val.t) in
   PMap.alloc ~new_codom:v_false st
 
-let free _addr _st = Symex.give_up ~loc:() "unimplemented: free"
+let free _addr _st = Symex.give_up ~loc:() "Free not implemented yet"
 let error msg _state = `Interp msg
