2eb85887dccf9dcb7694f9456477b902e7e55e8f
Sacha Ayoun <sachaayoun@gmail.com>
2025-11-23 16:48:22 +0000

    soteria-linear
    Signed-off-by: Sacha Ayoun <sachaayoun@gmail.com>

---
+++ soteria-linear/soteria_linear_lib.ml
@@ -0,0 +1,38 @@
+open Soteria
+open Soteria_std.Syntaxes.FunctionWrap
+open Soteria_linear_semantic
+module Lang = Soteria_linear_ast.Lang
+module Parser = Soteria_linear_parser.Parse
+
+let pp_results ft v =
+  let pp =
+    Fmt.Dump.list
+    @@ Fmt.Dump.pair
+         (Symex.Compo_res.pp ~ok:Interp.S_val.ppa
+            ~err:(Soteria.Symex.Or_gave_up.pp Fmt.string)
+            ~miss:Fmt.nop)
+         (Fmt.Dump.list Interp.S_val.ppa)
+  in
+  pp ft v
+
+let with_config logs_config solver_config f =
+  Soteria.Logs.Config.check_set_and_lock logs_config;
+  Soteria.Solvers.Config.set solver_config;
+  f ()
+
+module Exec_interp = Interp.Make (State)
+
+let exec file =
+  let program = Parser.parse_file file in
+  Fmt.pr "@[<v 2>Parsed program:@ %a@]@." Lang.Program.pp program;
+  let main =
+    match Lang.String_map.find_opt "main" program with
+    | Some f -> f
+    | None -> failwith "No main function found"
+  in
+  let process = Exec_interp.run_function main [] in
+  let results =
+    let@ () = Interp.with_program program in
+    Interp.Symex.Result.run ~fuel:Symex.Fuel_gauge.infinite ~mode:OX process
+  in
+  Fmt.pr "@[<v 2>Program executed with result:@ %a@]@?" pp_results results
