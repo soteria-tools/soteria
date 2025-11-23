003b1dc75abc3b34370d41725a1405cb1adad3fc
Sacha Ayoun <sachaayoun@gmail.com>
2025-11-23 17:49:19 +0000

    improve on excl
    Signed-off-by: Sacha Ayoun <sachaayoun@gmail.com>

--- soteria/lib/sym_states/excl.ml
+++ soteria/lib/sym_states/excl.ml
@@ -1,32 +1,50 @@
 open Symex
 
-module Make (Symex : Symex.Base) = struct
-  type 'a t = 'a
-  type 'a serialized = 'a
+module type Elem = sig
+  module Symex : Symex.Base
 
-  let pp pp_value = pp_value
+  type t [@@deriving show]
 
-  let load (st : 'a t option) =
+  val fresh : unit -> t Symex.t
+end
+
+module Make (Symex : Symex.Base) (E : Elem with module Symex = Symex) = struct
+  open Symex
+  open Symex.Syntax
+
+  type t = E.t [@@deriving show { with_path = false }]
+  type serialized = E.t [@@deriving show { with_path = false }]
+
+  let unwrap st =
     match st with
-    | Some x -> Symex.Result.ok (x, st)
-    | None -> Symex.Result.miss []
+    | Some x -> Symex.Result.ok x
+    | None ->
+        let* v = E.fresh () in
+        Result.miss [ v ]
 
-  let store x (st : 'a t option) =
-    match st with
-    | Some _ -> Symex.Result.ok ((), Some x)
-    | None -> Symex.Result.miss []
+  let assert_exclusively_owned (st : t option) =
+    let** _ = unwrap st in
+    Result.ok ()
+
+  let load (st : t option) =
+    let++ x = unwrap st in
+    (x, st)
+
+  let store x (st : t option) =
+    let++ _ = unwrap st in
+    ((), Some x)
 
   let serialize serialize_val x = serialize_val x
   let pp_serialized = pp
 
   let iter_vars_serialized (i : 'a -> 'b Symex.Value.ty Var.iter_vars)
-      (x : 'a serialized) : 'b Symex.Value.ty Var.iter_vars =
+      (x : serialized) : 'b Symex.Value.ty Var.iter_vars =
    fun f -> i x f
 
   let subst_serialized subst_inner subst_var x = subst_inner subst_var x
 
-  let consume ~sem_eq (serialized : 'a serialized) (t : 'a t option) :
-      ('a t option, [> Symex.lfail ], 'a serialized) Symex.Result.t =
+  let consume ~sem_eq (serialized : serialized) (t : t option) :
+      (t option, [> Symex.lfail ], serialized) Symex.Result.t =
     let open Symex.Syntax in
     match t with
     | Some x ->
@@ -34,7 +52,7 @@ module Make (Symex : Symex.Base) = struct
         None
     | None -> Symex.Result.miss [ serialized ]
 
-  let produce (serialized : 'a serialized) (t : 'a t option) =
+  let produce (serialized : serialized) (t : t option) =
     match t with
     | None -> Symex.return (Some serialized)
     | Some _ -> Symex.vanish ()
