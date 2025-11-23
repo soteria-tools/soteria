6944ec91319c8c7a7feac3c1d9ff8d58da587edb
Sacha Ayoun <sachaayoun@gmail.com>
2025-11-23 19:17:32 +0000

    wip
    Signed-off-by: Sacha Ayoun <sachaayoun@gmail.com>

--- soteria/lib/sym_states/excl.ml
+++ soteria/lib/sym_states/excl.ml
@@ -1,27 +1,35 @@
 open Symex
 
-module type Elem = sig
-  module Symex : Symex.Base
+module Elem (Symex : Symex.Base) = struct
+  module type S = sig
+    type t [@@deriving show]
 
-  type t [@@deriving show]
-
-  val fresh : unit -> t Symex.t
-  val sem_eq : t -> t -> Symex.Value.sbool Symex.Value.t
+    val fresh : unit -> t Symex.t
+    val sem_eq : t -> t -> Symex.Value.sbool Symex.Value.t
+  end
 end
 
-module Make (Symex : Symex.Base) (E : Elem with module Symex = Symex) = struct
+module Make (Symex : Symex.Base) (E : Elem(Symex).S) = struct
   open Symex
   open Symex.Syntax
 
+  module Logic = struct
+    type pred = Owned [@@deriving show { with_path = false }]
+    type ins = unit [@@deriving show { with_path = false }]
+    type outs = E.t [@@deriving show { with_path = false }]
+  end
+
   type t = E.t [@@deriving show { with_path = false }]
-  type serialized = E.t [@@deriving show { with_path = false }]
+
+  type serialized = Logic.pred * Logic.ins * Logic.outs
+  [@@deriving show { with_path = false }]
 
   let unwrap st =
     match st with
     | Some x -> Symex.Result.ok x
     | None ->
         let* v = E.fresh () in
-        Result.miss [ v ]
+        Result.miss [ (Logic.Owned, (), v) ]
 
   let assert_exclusively_owned (st : t option) =
     let** _ = unwrap st in
@@ -35,26 +43,19 @@ module Make (Symex : Symex.Base) (E : Elem with module Symex = Symex) = struct
     let++ _ = unwrap st in
     ((), Some x)
 
-  let serialize x = x
-  let pp_serialized = pp
+  let serialize x = (Logic.Owned, (), x)
 
   let iter_vars_serialized (i : 'a -> 'b Symex.Value.ty Var.iter_vars)
-      (x : serialized) : 'b Symex.Value.ty Var.iter_vars =
+      ((Logic.Owned, (), x) : serialized) : 'b Symex.Value.ty Var.iter_vars =
    fun f -> i x f
 
   let subst_serialized subst_inner subst_var x = subst_inner subst_var x
 
-  let consume (serialized : serialized) (t : t option) :
-      (t option, [> Symex.lfail ], serialized) Symex.Result.t =
-    let open Symex.Syntax in
-    match t with
-    | Some x ->
-        let++ () = Symex.consume_pure (E.sem_eq x serialized) in
-        None
-    | None -> Symex.Result.miss [ serialized ]
+  let consume Logic.Owned (() : Logic.ins) (t : t option) :
+      (Logic.outs * t option, [> Symex.lfail ], serialized) Symex.Result.t =
+    let++ x = unwrap t in
+    (x, None)
 
-  let produce (serialized : serialized) (t : t option) =
-    match t with
-    | None -> Symex.return (Some serialized)
-    | Some _ -> Symex.vanish ()
+  let produce ((Owned, (), v) : serialized) (t : t option) =
+    match t with None -> Symex.return (Some v) | Some _ -> Symex.vanish ()
 end
