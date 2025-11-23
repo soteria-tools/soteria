a091371771208234da36c060f7b769f551ad01eb
Sacha Ayoun <sachaayoun@gmail.com>
2025-11-23 17:57:38 +0000

    add freeing
    Signed-off-by: Sacha Ayoun <sachaayoun@gmail.com>

--- soteria-linear/semantic/state.ml
+++ soteria-linear/semantic/state.ml
@@ -5,19 +5,27 @@ type err = [ `UseAfterFree | `Interp of string ]
 
 module PMap = Soteria.Sym_states.Pmap.Make (Symex) (S_int)
 module Excl_val = Soteria.Sym_states.Excl.Make (Symex) (S_val)
+module Freeable = Soteria.Sym_states.Freeable.Make (Symex)
 
-type t = Excl_val.t PMap.t option [@@deriving show { with_path = false }]
+type t = Excl_val.t Freeable.t PMap.t option
+[@@deriving show { with_path = false }]
 
-type fixes = Excl_val.serialized PMap.serialized
+type fixes = Excl_val.serialized Freeable.serialized PMap.serialized
 [@@deriving show { with_path = false }]
 
 let empty : t = None
-let load addr st = PMap.wrap Excl_val.load addr st
-let store addr value st = PMap.wrap (Excl_val.store value) addr st
+let load addr st = PMap.wrap (Freeable.wrap Excl_val.load) addr st
+
+let store addr value st =
+  PMap.wrap (Freeable.wrap (Excl_val.store value)) addr st
 
 let alloc st =
   let v_false = (S_val.v_false :> S_val.t) in
-  PMap.alloc ~new_codom:v_false st
+  PMap.alloc ~new_codom:(Freeable.Alive v_false) st
 
-let free _addr _st = Symex.give_up ~loc:() "Free not implemented yet"
+let free addr st =
+  PMap.wrap
+    (Freeable.free ~assert_exclusively_owned:Excl_val.assert_exclusively_owned)
+    addr st
+
 let error msg _state = `Interp msg
