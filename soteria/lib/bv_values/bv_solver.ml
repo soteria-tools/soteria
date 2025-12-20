1bc4cd244a99beaf2632af482624974415151068
N1ark <opale.sjostedt@gmail.com>
2025-12-20 11:46:42 +0000

    Remove `Declared_vars` from non-incr `Bv_solver`

--- soteria/lib/bv_values/bv_solver.ml
+++ soteria/lib/bv_values/bv_solver.ml
@@ -368,45 +368,9 @@ struct
       (to_encode, var_set)
   end
 
-  module Declared_vars = struct
-    module Var_counter = Var.Incr_counter_mut (struct
-      let start_at = 1
-    end)
-
-    (* Since we start addresses at one to improve trivial model hits, we need to offset to obtain an index. *)
-    let var_to_index v = Var.to_int v - 1
-
-    type t = {
-      counter : Var_counter.t;
-      types : Svalue.ty Dynarray.t;
-          (** The var_counter is keeping track of how many variables we actually
-              have in the context. We don't need to separate each bit of that
-              array by saved branch, we just fetch at the index for each
-              variable, and override when we change branch. *)
-    }
-
-    let init () = { counter = Var_counter.init (); types = Dynarray.create () }
-    let save t = Var_counter.save t.counter
-    let backtrack_n t n = Var_counter.backtrack_n t.counter n
-    let reset t = Var_counter.reset t.counter
-
-    let fresh t ty =
-      let next = Var_counter.get_next t.counter in
-      let next_i = var_to_index next in
-      let () =
-        if Dynarray.length t.types == next_i then Dynarray.add_last t.types ty
-        else if Dynarray.length t.types > next_i then
-          Dynarray.set t.types next_i ty
-        else failwith "Broke var-counter/declared-types invariant"
-      in
-      next
-
-    let get_ty t var = Dynarray.get t.types (var_to_index var)
-  end
-
   type t = {
     z3_exe : Intf.t;
-    vars : Declared_vars.t;
+    vars : Var_counter.t;
     save_counter : Save_counter.t;
     state : Solver_state.t;
     analysis : Analysis.t;
@@ -418,19 +382,19 @@ struct
     {
       z3_exe;
       save_counter = Save_counter.init ();
-      vars = Declared_vars.init ();
+      vars = Var_counter.init ();
       state = Solver_state.init ();
       analysis = Analysis.init ();
     }
 
   let save solver =
-    Declared_vars.save solver.vars;
+    Var_counter.save solver.vars;
     Save_counter.save solver.save_counter;
     Solver_state.save solver.state;
     Analysis.save solver.analysis
 
   let backtrack_n solver n =
-    Declared_vars.backtrack_n solver.vars n;
+    Var_counter.backtrack_n solver.vars n;
     Solver_state.backtrack_n solver.state n;
     Save_counter.backtrack_n solver.save_counter n;
     Analysis.backtrack_n solver.analysis n
@@ -442,12 +406,11 @@ struct
     let save_counter = !(solver.save_counter) in
     if save_counter < 0 then failwith "Solver reset: save_counter < 0???";
     Save_counter.reset solver.save_counter;
-    Declared_vars.reset solver.vars;
+    Var_counter.reset solver.vars;
     Solver_state.reset solver.state;
     Analysis.reset solver.analysis
 
-  let fresh_var solver ty =
-    Declared_vars.fresh solver.vars (Typed.untype_type ty)
+  let fresh_var solver _ = Var_counter.get_next solver.vars
 
   let simplify solver v : 'a Typed.t =
     v
@@ -498,30 +461,29 @@ struct
     let res = Eval.eval ~eval_var to_check in
     Svalue.equal res Svalue.Bool.v_true
 
-  let check_sat_raw solver relevant_vars to_check =
+  let check_sat_raw solver to_check =
     (* TODO: we shouldn't wait for ack for each command individually... *)
+    let var_tys =
+      Svalue.iter_vars to_check
+      |> Iter.fold (fun acc (v, ty) -> Var.Map.add v ty acc) Var.Map.empty
+    in
     if trivial_model_works to_check then Symex.Solver_result.Sat
     else (
       (* We need to reset the state, so we can push the new constraints *)
       Intf.reset solver.z3_exe;
-
       (* Declare all relevant variables *)
-      Var.Hashset.iter
-        (fun v ->
-          let ty = Declared_vars.get_ty solver.vars v in
-          Intf.declare_var solver.z3_exe v ty)
-        relevant_vars;
+      Var.Map.iter (Intf.declare_var solver.z3_exe) var_tys;
       (* Declare the constraint *)
       Intf.add_constraint solver.z3_exe to_check;
       (* Actually check sat *)
       Intf.check_sat solver.z3_exe)
 
-  let check_sat_raw_memo solver relevant_vars to_check =
+  let check_sat_raw_memo solver to_check =
     let to_check = Typed.untyped to_check in
     match Hashtbl.Hint.find_opt memo_sat_check_tbl to_check.Hc.tag with
     | Some result -> result
     | None ->
-        let result = check_sat_raw solver relevant_vars to_check in
+        let result = check_sat_raw solver to_check in
         Hashtbl.Hint.add memo_sat_check_tbl to_check.Hc.tag result;
         result
 
@@ -539,7 +501,7 @@ struct
           Iter.fold Typed.and_ to_check
             (Analysis.encode ~vars:relevant_vars solver.analysis)
         in
-        let answer = check_sat_raw_memo solver relevant_vars to_check in
+        let answer = check_sat_raw_memo solver to_check in
         if answer = Sat then Solver_state.mark_checked solver.state;
         answer
 
