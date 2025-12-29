cf1b9f91e8dd35c2039e1fa6939f3c31332d225c
N1ark <opale.sjostedt@gmail.com>
2025-12-29 21:48:53 +0100

    Update print_stats.ml

--- soteria/tests/symex/print_stats.ml
+++ soteria/tests/symex/print_stats.ml
@@ -1,10 +1,10 @@
 open
   Soteria.Symex.Make
     (Soteria.Symex.Meta.Dummy)
-    (Soteria.C_values.C_solver.Z3_solver)
+    (Soteria.Tiny_values.Tiny_solver.Z3_solver)
 
 open Syntax
-open Soteria.C_values.Typed
+open Soteria.Tiny_values.Typed
 
 let process : (int, int, unit) Result.t =
   let* b1 = nondet t_bool in
