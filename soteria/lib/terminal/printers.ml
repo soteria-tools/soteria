e37eddc81de754edaf9fc4bad44ff6f57309667a
opale <opale.sjostedt@gmail.com>
2026-01-11 17:34:20 +0100

    Share stats printing, improve `Soteria.Terminal` (#209)
    * Add `Terminal.Printers`
    
    * Add shared stats output flag
    
    * Add `pp_plural`
    
    * Add `--plugin-directory` for Rust
    
    * Tests: always have `--hide-unstable --clean`
    
    * Update print_stats.ml
    
    * Fix detection of dune test
    
    * driveby: comment out unused tokens
    
    * Remove test environment detection
    
    * Comment
    
    * PR comment
    
    * Update printers.ml
    
    ---------
    
    Co-authored-by: Sacha Ayoun <sachaayoun@gmail.com>

---
+++ soteria/lib/terminal/printers.ml
@@ -0,0 +1,23 @@
+(** Wraps a pretty-printer for unstable values, that should not be output into
+    e.g. testing environments where diffs matter. If the config option
+    [hide_unstable] is set, will replace *)
+let pp_unstable ~name pp ft x =
+  if (Config.get ()).hide_unstable then Fmt.pf ft "<%s>" name else pp ft x
+
+(** Pretty-prints a time quantity in seconds, displaying it in either seconds or
+    milli-seconds for smaller values (< 0.05s). Wrapped in {!pp_unstable}. *)
+let pp_time =
+  let pp_time ft t =
+    if t < 0.05 then Fmt.pf ft "%ams" (Fmt.float_dfrac 2) (t *. 1000.)
+    else Fmt.pf ft "%as" (Fmt.float_dfrac 2) t
+  in
+  pp_unstable ~name:"time" pp_time
+
+(** For a given value [(total, part)], prints what percentage of [total] is
+    represented by [part]. e.g. [pp_percent stdout (5, 1)] will print [20.00%]
+*)
+let pp_percent ft (total, part) = (Fmt.float_dfrac 2) ft (100. *. part /. total)
+
+(** Prints [1 sing] if [n = 1], [n plur] otherwise. *)
+let pp_plural ~sing ~plur ft n =
+  Fmt.pf ft "%d %s" n (if n = 1 then sing else plur)
