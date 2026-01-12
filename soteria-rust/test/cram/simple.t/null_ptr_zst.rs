ac44807609d1f5ba05ddee171c8d6b8292d03e0d
N1ark <opale.sjostedt@gmail.com>
2026-01-12 16:52:42 +0100

    Update tests

--- soteria-rust/test/cram/simple.t/null_ptr_zst.rs
+++
@@ -1,6 +0,0 @@
-#[rusteria::test]
-fn null_ptr_zst() {
-    let ptr: *const () = std::ptr::null();
-    // this shouldn't fail; reading a ZST through a null pointer is allowed
-    let zst: () = unsafe { *ptr };
-}
