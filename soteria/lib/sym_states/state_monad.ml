5b279f5773b2a5d1f027d137af572a3a325f02e8
Sacha Ayoun <sachaayoun@gmail.com>
2026-01-25 16:47:24 +0000

    Upgrade state models to use a state monad by default
    Signed-off-by: Sacha Ayoun <sachaayoun@gmail.com>

--- soteria/lib/sym_states/state_monad.ml
+++ soteria/lib/sym_states/state_monad.ml
@@ -9,6 +9,7 @@ module type S = sig
 
   val lift : 'a Symex.t -> 'a t
   val get_state : unit -> st t
+  val set_state : st -> unit t
   val map_state : (st -> st) -> unit t
   val with_state : state:st -> 'a t -> 'a t
   val run_with_state : state:st -> 'a t -> ('a * st) Symex.t
@@ -17,7 +18,7 @@ module type S = sig
     include module type of Result
 
     val run_with_state :
-      state:st -> ('a, 'e, 'f) t -> ('a * st, 'e, 'f) Symex.Result.t
+      state:st -> ('a, 'e, 'f) t -> ('a * st, 'e * st, 'f) Symex.Result.t
   end
 
   module Syntax : sig
@@ -36,6 +37,19 @@ module type S = sig
   end
 end
 
+module Tys
+    (Sym : Symex.Base)
+    (State : sig
+      type t
+    end) =
+struct
+  type 'a t = State.t -> ('a * State.t) Sym.t
+
+  module Result = struct
+    type nonrec ('a, 'e, 'f) t = ('a, 'e, 'f) Compo_res.t t
+  end
+end
+
 module Make
     (Sym : Symex.Base)
     (State : sig
@@ -51,67 +65,77 @@ module Make
   module Symex = Sym
   module Value = Sym.Value
   module Stats = Sym.Stats
+  module Tys = Tys (Sym) (State)
 
   type 'a t = State.t -> ('a * State.t) Sym.t
   type lfail = Sym.lfail
   type st = State.t
 
-  let return x st = Sym.return (x, st)
+  let[@inline] return x st = Sym.return (x, st)
 
-  let bind m f st =
+  let[@inline] bind m f st =
     let* x, st' = m st in
     f x st'
 
-  let map m f st =
+  let[@inline] map m f st =
     let+ x, st' = m st in
     (f x, st')
 
-  let lift x st =
+  let[@inline] lift x st =
     let+ x in
     (x, st)
 
-  let get_state () st = Sym.return (st, st)
-  let map_state f st = Sym.return ((), f st)
+  let[@inline] run_with_state ~state x = x state
 
-  let with_state ~state x =
+  let[@inline] with_state ~state x =
    fun st ->
     let+ res, _ = x state in
     (res, st)
 
-  let run_with_state ~state x = x state
-  let assume b = lift (Sym.assume b)
-  let vanish () = lift (Sym.vanish ())
-  let assert_ b = lift (Sym.assert_ b)
-  let consume_pure b = lift (Sym.consume_pure b)
-  let consume_false () = lift (Sym.consume_false ())
-  let nondet ty = lift (Sym.nondet ty)
-  let simplify v = lift (Sym.simplify v)
-  let fresh_var ty = lift (Sym.fresh_var ty)
-  let assert_or_error b err = lift (Sym.assert_or_error b err)
-  let branches b st = Sym.branches (List.map (fun f () -> f () st) b)
-  let consume_fuel_steps n = lift (Sym.consume_fuel_steps n)
-  let give_up ~loc msg = lift (Sym.give_up ~loc msg)
-  let some_or_give_up ~loc msg x = lift (Sym.some_or_give_up ~loc msg x)
-  let all fn xs = Monad.all fn xs ~return ~bind
-  let foldM ~fold x ~init ~f = Monad.foldM ~bind ~return ~fold x ~init ~f
-  let fold_list x ~init ~f = foldM ~fold:Foldable.List.fold x ~init ~f
-  let fold_iter x ~init ~f = foldM ~fold:Foldable.Iter.fold x ~init ~f
-  let fold_seq x ~init ~f = foldM ~fold:Foldable.Seq.fold x ~init ~f
+  let[@inline] get_state () = fun st -> Symex.return (st, st)
+  let[@inline] set_state st = fun _ -> Symex.return ((), st)
+  let[@inline] map_state f st = Sym.return ((), f st)
+  let[@inline] assume b = lift (Sym.assume b)
+  let[@inline] vanish () = lift (Sym.vanish ())
+  let[@inline] assert_ b = lift (Sym.assert_ b)
+  let[@inline] consume_pure b = lift (Sym.consume_pure b)
+  let[@inline] consume_false () = lift (Sym.consume_false ())
+  let[@inline] nondet ty = lift (Sym.nondet ty)
+  let[@inline] simplify v = lift (Sym.simplify v)
+  let[@inline] fresh_var ty = lift (Sym.fresh_var ty)
+  let[@inline] assert_or_error b err = lift (Sym.assert_or_error b err)
+  let[@inline] branches b st = Sym.branches (List.map (fun f () -> f () st) b)
+  let[@inline] consume_fuel_steps n = lift (Sym.consume_fuel_steps n)
+  let[@inline] give_up ~loc msg = lift (Sym.give_up ~loc msg)
 
-  let branch_on ?left_branch_name ?right_branch_name guard ~then_ ~else_ =
+  let[@inline] some_or_give_up ~loc msg x =
+    lift (Sym.some_or_give_up ~loc msg x)
+
+  let[@inline] all fn xs = Monad.all fn xs ~return ~bind
+
+  let[@inline] foldM ~fold x ~init ~f =
+    Monad.foldM ~bind ~return ~fold x ~init ~f
+
+  let[@inline] fold_list x ~init ~f = foldM ~fold:Foldable.List.fold x ~init ~f
+  let[@inline] fold_iter x ~init ~f = foldM ~fold:Foldable.Iter.fold x ~init ~f
+  let[@inline] fold_seq x ~init ~f = foldM ~fold:Foldable.Seq.fold x ~init ~f
+
+  let[@inline] branch_on ?left_branch_name ?right_branch_name guard ~then_
+      ~else_ =
    fun st ->
     Sym.branch_on ?left_branch_name ?right_branch_name guard
       ~then_:(fun () -> then_ () st)
       ~else_:(fun () -> else_ () st)
 
-  let branch_on_take_one ?left_branch_name ?right_branch_name guard ~then_
-      ~else_ =
+  let[@inline] branch_on_take_one ?left_branch_name ?right_branch_name guard
+      ~then_ ~else_ =
    fun st ->
     Sym.branch_on_take_one ?left_branch_name ?right_branch_name guard
       ~then_:(fun () -> then_ () st)
       ~else_:(fun () -> else_ () st)
 
-  let if_sure ?left_branch_name ?right_branch_name guard ~then_ ~else_ =
+  let[@inline] if_sure ?left_branch_name ?right_branch_name guard ~then_ ~else_
+      =
    fun st ->
     Sym.if_sure ?left_branch_name ?right_branch_name guard
       ~then_:(fun () -> then_ () st)
@@ -126,17 +150,25 @@ module Make
       let map = map
     end)
 
-    let run_with_state ~state x =
+    let[@inline] run_with_state ~state x =
       Symex.map (x state) @@ function
       | Compo_res.Ok res, state -> Compo_res.Ok (res, state)
-      | Error e, _ -> Error e
+      | Error e, state -> Error (e, state)
       | Missing f, _ -> Missing f
 
-    let miss_no_fix ~reason () = lift (Sym.Result.miss_no_fix ~reason ())
-    let foldM ~fold x ~init ~f = Monad.foldM ~bind ~return:ok ~fold x ~init ~f
-    let fold_list x ~init ~f = foldM ~fold:Foldable.List.fold x ~init ~f
-    let fold_iter x ~init ~f = foldM ~fold:Foldable.Iter.fold x ~init ~f
-    let fold_seq x ~init ~f = foldM ~fold:Foldable.Seq.fold x ~init ~f
+    let[@inline] miss_no_fix ~reason () =
+      lift (Sym.Result.miss_no_fix ~reason ())
+
+    let[@inline] foldM ~fold x ~init ~f =
+      Monad.foldM ~bind ~return:ok ~fold x ~init ~f
+
+    let[@inline] fold_list x ~init ~f =
+      foldM ~fold:Foldable.List.fold x ~init ~f
+
+    let[@inline] fold_iter x ~init ~f =
+      foldM ~fold:Foldable.Iter.fold x ~init ~f
+
+    let[@inline] fold_seq x ~init ~f = foldM ~fold:Foldable.Seq.fold x ~init ~f
   end
 
   module Syntax = struct
@@ -146,10 +178,10 @@ module Make
     let ( let++ ) = Result.map
     let ( let+- ) = Result.map_error
     let ( let+? ) = Result.map_missing
-    let ( let*^ ) x f = bind (lift x) f
-    let ( let+^ ) x f = map (lift x) f
-    let ( let**^ ) x f = Result.bind (lift x) f
-    let ( let++^ ) x f = Result.map (lift x) f
+    let[@inline] ( let*^ ) x f = bind (lift x) f
+    let[@inline] ( let+^ ) x f = map (lift x) f
+    let[@inline] ( let**^ ) x f = Result.bind (lift x) f
+    let[@inline] ( let++^ ) x f = Result.map (lift x) f
 
     module Symex_syntax = struct
       let branch_on = branch_on
