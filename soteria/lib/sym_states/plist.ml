2e06f26dceb06f0df3762373693b32c800f87b29
Sacha Ayoun <sachaayoun@gmail.com>
2026-01-29 21:35:09 +0000

    Re-establish pmist.ml
    Signed-off-by: Sacha Ayoun <sachaayoun@gmail.com>

--- soteria/lib/sym_states/plist.ml
+++ soteria/lib/sym_states/plist.ml
@@ -22,39 +22,61 @@ module SInt_sig (Symex : Symex.Base) = struct
   end
 end
 
-module Make (Symex : Symex.Base) (SInt : SInt_sig(Symex).S) = struct
+module Make
+    (Symex : Symex.Base)
+    (SInt : SInt_sig(Symex).S)
+    (Elem : Base.M(Symex).S) =
+struct
   open Symex.Syntax
-  open Symex
   module M = Stdlib.Map.Make (SInt)
 
-  type 'a t = 'a M.t * SInt.t option
-  type 'a serialized = (SInt.t * 'a) list * SInt.t option
+  type t = Elem.t M.t * SInt.t option
+
+  module SM =
+    State_monad.Make
+      (Symex)
+      (struct
+        type nonrec t = t option
+      end)
+
+  type serialized =
+    | Ser_binding of (SInt.t * Elem.serialized)
+    | Ser_bound of SInt.t
 
   let no_fix = []
-  let[@inline] lift_fix ~ofs fix = ([ (ofs, fix) ], None)
-  let[@inline] lift_fixes ~ofs fixes = List.map (lift_fix ~ofs) fixes
+  let[@inline] lift_fix ~ofs (fix : Elem.serialized) = Ser_binding (ofs, fix)
+  let[@inline] lift_fixes ~ofs = List.map @@ List.map (lift_fix ~ofs)
   let of_opt = function None -> (M.empty, None) | Some l -> l
   let[@inline] add_opt k v m = M.update k (fun _ -> v) m
 
   let to_opt (m, b) =
     if Option.is_none b && M.is_empty m then None else Some (m, b)
 
-  let pp pp_value ft ((m, b) : 'a t) =
-    let pp_binding ft (k, v) = Fmt.pf ft "%a -> %a" SInt.pp k pp_value v in
+  let pp' ?(elem = Elem.pp) ft ((m, b) : t) =
+    let pp_binding ft (k, v) = Fmt.pf ft "%a -> %a" SInt.pp k elem v in
     Fmt.pf ft "BOUND: %a ; {%a}" (Fmt.Dump.option SInt.pp) b
       (Fmt.list ~sep:(Fmt.any "; ") pp_binding)
       (M.bindings m)
 
-  let serialize serialize_inner (m, b) =
-    let bindings =
-      M.to_seq m |> Seq.map (fun (k, v) -> (k, serialize_inner v))
-    in
-    (List.of_seq bindings, b)
+  let pp ft t = pp' ~elem:Elem.pp ft t
 
-  let pp_serialized pp_inner : Format.formatter -> 'a serialized -> unit =
-   fun ft (l, b) ->
-    let m = M.of_seq (List.to_seq l) in
-    pp pp_inner ft (m, b)
+  let serialize (m, b) : serialized list =
+    let bindings =
+      M.to_seq m
+      |> Seq.concat_map (fun (k, v) ->
+          Elem.serialize v
+          |> List.to_seq
+          |> Seq.map (fun v -> Ser_binding (k, v)))
+      |> List.of_seq
+    in
+    match b with None -> bindings | Some b -> Ser_bound b :: bindings
+
+  let pp_serialized : Format.formatter -> serialized -> unit =
+   fun ft ser ->
+    match ser with
+    | Ser_binding (k, v) ->
+        Fmt.pf ft "Binding: %a -> %a" SInt.pp k Elem.pp_serialized v
+    | Ser_bound b -> Fmt.pf ft "Bound: %a" SInt.pp b
 
   let empty = M.empty
 
@@ -77,36 +99,44 @@ module Make (Symex : Symex.Base) (SInt : SInt_sig(Symex).S) = struct
       | None -> SInt.greater_or_equal ofs (SInt.of_int 0)
       | Some b -> SInt.in_range ofs (SInt.of_int 0, b)
     in
-    if%sat Symex.Value.not cond then Result.error `OutOfBounds else Result.ok ()
+    SM.lift @@ Symex.assert_or_error cond `OutOfBounds
 
-  let create (size : int) ~(new_codom : 'a) : 'a t =
+  let create (size : int) ~(new_codom : Elem.t) : t =
     if size <= 0 then
       raise (Invalid_argument "Plist.create: size must be positive");
     let bindings = Seq.init size (fun i -> (SInt.of_int i, new_codom)) in
     let m = M.of_seq bindings in
     (m, Some (SInt.of_int size))
 
-  let assert_exclusively_owned (b_opt : 'a t option) =
-    let err = Result.miss no_fix in
+  open SM
+  open SM.Syntax
+
+  let assert_exclusively_owned () : (unit, 'err, serialized list) SM.Result.t =
+    let err = SM.Result.miss no_fix in
+    let* b_opt = SM.get_state () in
     match of_opt b_opt with
     | _, None -> err
     | m, Some b ->
         if%sat SInt.sem_eq b (SInt.of_int (M.cardinal m)) then Result.ok ()
         else err
 
-  let wrap (f : 'a option -> ('b * 'a option, 'err, 'fix) Symex.Result.t)
-      (ofs : SInt.t) (t_opt : 'a t option) :
-      ('b * 'a t option, 'err, 'fix serialized) Symex.Result.t =
+  let wrap (ofs : SInt.t)
+      (f : ('b, 'err, Elem.serialized list) Elem.SM.Result.t) :
+      ('b, 'err, serialized list) SM.Result.t =
+    let* t_opt = SM.get_state () in
     let m, b = of_opt t_opt in
     let** () = assert_in_bounds ofs b in
-    let* ofs, sst = find_opt_sym ofs m in
-    let+ res = f sst in
+    let*^ ofs, sst = find_opt_sym ofs m in
+    let*^ res, sst = f sst in
     match res with
-    | Ok (v, sst') -> Ok (v, to_opt (add_opt ofs sst' m, b))
-    | Error e -> Error e
-    | Missing fixes -> Missing (lift_fixes ~ofs fixes)
+    | Ok v ->
+        (* Only update state in case of success *)
+        let+ () = SM.set_state (to_opt (add_opt ofs sst m, b)) in
+        Ok v
+    | Error e -> SM.Result.error e
+    | Missing fixes -> SM.Result.miss (lift_fixes ~ofs fixes)
 
-  let consume
+  (* let consume
       (cons :
         'inner_serialized ->
         'inner_st option ->
@@ -139,44 +169,40 @@ module Make (Symex : Symex.Base) (SInt : SInt_sig(Symex).S) = struct
           in
           add_opt ofs codom m)
     in
-    to_opt (m, new_b)
+    to_opt (m, new_b) *)
 
-  let produce
-      (prod : 'inner_serialized -> 'inner_st option -> 'inner_st option Symex.t)
-      (serialized : 'inner_serialized serialized) (st : 'inner_st t option) =
+  let produce (serialized : serialized) : unit SM.t =
+    let* st = SM.get_state () in
     let m, b = of_opt st in
-    let l, b_ser = serialized in
-    let* new_b =
-      match (b, b_ser) with
-      | None, None -> Symex.return None
-      | (Some _ as x), None | None, (Some _ as x) -> Symex.return x
-      | Some _, Some _ -> Symex.vanish ()
-    in
     let in_bounds_opt x =
-      match new_b with
+      match b with
       | None -> SInt.greater_or_equal x (SInt.of_int 0)
       | Some b -> SInt.in_range x (SInt.of_int 0, b)
     in
-    let+ m =
-      Symex.fold_list l ~init:m ~f:(fun m (ofs, inner_ser) ->
-          let* () = Symex.assume [ in_bounds_opt ofs ] in
-          let* ofs, codom = find_opt_sym ofs m in
-          let+ codom = prod inner_ser codom in
-          add_opt ofs codom m)
-    in
-    to_opt (m, new_b)
+    match serialized with
+    | Ser_bound b_ser -> (
+        match b with
+        | None -> SM.set_state (to_opt (m, Some b_ser))
+        | Some _ -> SM.vanish ())
+    | Ser_binding (ofs, inner_ser) ->
+        let* () = SM.assume [ in_bounds_opt ofs ] in
+        let*^ ofs, codom = find_opt_sym ofs m in
+        let*^ (), codom = Elem.produce inner_ser codom in
+        let m = add_opt ofs codom m in
+        SM.set_state (to_opt (m, b))
 
-  let subst_serialized subst_inner subst_var (l, b) =
+  let subst_serialized subst_var (l, b) =
     ( List.map
-        (fun (k, v) -> (SInt.subst subst_var k, subst_inner subst_var v))
+        (fun (k, v) ->
+          (SInt.subst subst_var k, Elem.subst_serialized subst_var v))
         l,
       Option.map (SInt.subst subst_var) b )
 
-  let iter_vars_serialized iter_inner (l, b) f =
+  let iter_vars_serialized (l, b) f =
     Option.iter (fun b -> SInt.iter_vars b f) b;
     List.iter
       (fun (k, v) ->
         SInt.iter_vars k f;
-        iter_inner v f)
+        Elem.iter_vars_serialized v f)
       l
 end
