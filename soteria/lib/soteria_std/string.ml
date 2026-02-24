6c435d2d6aed51bc6b81bf951f2cdcf2da4b3cf7
Sacha Ayoun <sachaayoun@gmail.com>
2026-02-24 22:15:53 +0000

    Add string interning
    Signed-off-by: Sacha Ayoun <sachaayoun@gmail.com>

--- soteria/lib/soteria_std/string.ml
+++ soteria/lib/soteria_std/string.ml
@@ -12,3 +12,26 @@ let index_of ~sub_str s =
   with Not_found -> None
 
 let contains ~sub_str s = Option.is_some (index_of ~sub_str s)
+
+module Interned : sig
+  type t
+
+  val intern : string -> t
+  val to_string : t -> string
+
+  (** Unique tag of the interned string. *)
+  val tag : t -> int
+end = struct
+  module HC = Hc.Make_strong_thread_safe (struct
+    type t = string
+
+    let hash = Stdlib.Hashtbl.hash
+    let equal = Stdlib.String.equal
+  end)
+
+  type t = string Hc.hash_consed
+
+  let intern s = HC.hashcons s
+  let to_string hc = hc.Hc.node
+  let tag hc = hc.Hc.tag
+end
--- soteria/lib/terminal/warn.ml
+++ soteria/lib/terminal/warn.ml
@@ -1,18 +1,20 @@
 open Soteria_std
 
 module WarnHSet = Hashset.Make (struct
-  type t = string * Diagnostic.severity [@@deriving eq, ord, show]
+  type t = String.Interned.t
 
-  let hash = Hashtbl.hash
+  let hash s = Int.hash @@ String.Interned.tag s
+  let equal s1 s2 = String.Interned.tag s1 = String.Interned.tag s2
+  let pp ft s = Fmt.string ft (String.Interned.to_string s)
 end)
 
 let issued_warnings = WarnHSet.with_capacity 8
-let was_issued ~severity msg = WarnHSet.mem issued_warnings (msg, severity)
+let was_issued msg = WarnHSet.mem issued_warnings msg
+let warn msg = Diagnostic.print_diagnostic_simple ~severity:Warning msg
 
-let warn ?(severity = Diagnostic.Warning) msg =
-  Diagnostic.print_diagnostic_simple ~severity msg
-
-let warn_once ?(severity = Diagnostic.Warning) msg =
-  if not (was_issued ~severity msg) then (
-    WarnHSet.add issued_warnings (msg, severity);
-    warn ~severity msg)
+(** Like warn but receives an interned string and only warns once per identical
+    string. *)
+let warn_once msg =
+  if not (was_issued msg) then (
+    WarnHSet.add issued_warnings msg;
+    warn (String.Interned.to_string msg))
