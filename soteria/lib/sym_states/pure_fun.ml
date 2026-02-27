05cc3178854c26d48e075f92ed523d0e8656c9bc
Sacha Ayoun <sachaayoun@gmail.com>
2026-02-27 16:44:44 +0000

    pure fun
    Signed-off-by: Sacha Ayoun <sachaayoun@gmail.com>

--- soteria/lib/sym_states/pure_fun.ml
+++ soteria/lib/sym_states/pure_fun.ml
@@ -9,18 +9,11 @@ open Compo_res
 
 (** FIXME: This is almost verbatim the same thing as the input of excl *)
 module Codom (Symex : Symex.Base) = struct
-  open Symex
+  module Data = Data.M (Symex)
 
   module type S = sig
-    type t [@@deriving show]
-    type syn [@@deriving show]
-
-    val fresh : unit -> t Symex.t
-    val sem_eq : t -> t -> Symex.Value.(sbool t)
-    val to_syn : t -> syn
-    val subst : (Value.Expr.t -> 'a Value.t) -> syn -> t
-    val learn_eq : syn -> t -> (unit, 'a) Symex.Consumer.t
-    val exprs_syn : syn -> Symex.Value.Expr.t list
+    include Data.Abstr_with_syn
+    include Data.Sem_eq with type t := t and type sbool := Symex.Value.(sbool t)
   end
 end
 
@@ -29,14 +22,6 @@ module Make (Symex : Symex.Base) (C : Codom(Symex).S) = struct
 
   type t = C.t [@@deriving show]
   type syn = C.syn [@@deriving show]
-
-  let produce (s : syn) (t : E.t option) : E.t option Producer.t =
-    let open Producer.Syntax in
-    match t with
-    | None ->
-        let+ x = Producer.apply_subst E.subst s in
-        Some x
-    | Some _ -> Producer.vanish ()
 
   module SM =
     State_monad.Make
@@ -48,9 +33,10 @@ module Make (Symex : Symex.Base) (C : Codom(Symex).S) = struct
   open SM
   open SM.Syntax
 
-  let to_syn s = [ s ]
+  let to_syn s = [ C.to_syn s ]
+  let ins_outs s = ([], C.exprs_syn s)
   let pp = C.pp
-  let pp_syn = pp
+  let pp_syn = C.pp_syn
 
   let load () =
     let* st = SM.get_state () in
@@ -63,4 +49,13 @@ module Make (Symex : Symex.Base) (C : Codom(Symex).S) = struct
 
   (* let consume (syn : syn) = let** t = load () in lift @@ Symex.consume_pure
      (sem_eq t syn) *)
+  open Symex
+
+  let produce (s : syn) (t : st) : st Producer.t =
+    let open Producer.Syntax in
+    match t with
+    | None ->
+        let+ x = Producer.apply_subst C.subst s in
+        Some x
+    | Some _ -> Producer.vanish ()
 end
