c66ee2bd8f12bf1ec4d36aa713f545f94fce1487
Sacha Ayoun <sachaayoun@gmail.com>
2026-02-27 15:27:28 +0000

    plist
    Signed-off-by: Sacha Ayoun <sachaayoun@gmail.com>

--- soteria/lib/sym_states/plist.ml
+++ soteria/lib/sym_states/plist.ml
@@ -2,23 +2,19 @@ open Symex
 open Compo_res
 
 module SInt_sig (Symex : Symex.Base) = struct
+  module Data = Data.M (Symex)
+
   module type S = sig
     (** Symbolic integers *)
     open Symex
 
-    type t
-
+    include Data.Abstr_with_syn
     include Stdlib.Map.OrderedType with type t := t
+    include Data.Sem_eq with type t := t and type sbool := Value.(sbool t)
 
-    type sbool_v := Value.(sbool t)
-
-    val pp : Format.formatter -> t -> unit
-    val sem_eq : t -> t -> sbool_v
     val of_int : int -> t
-    val in_range : t -> t * t -> sbool_v
-    val greater_or_equal : t -> t -> sbool_v
-    val subst : (Var.t -> Var.t) -> t -> t
-    val iter_vars : t -> (Var.t * 'a Value.ty -> unit) -> unit
+    val in_range : t -> t * t -> Value.(sbool t)
+    val greater_or_equal : t -> t -> Value.(sbool t)
   end
 end
 
@@ -39,10 +35,19 @@ struct
         type nonrec t = t option
       end)
 
-  type syn = Ser_binding of (SInt.t * Elem.syn) | Ser_bound of SInt.t
+  type syn = Ser_binding of (SInt.syn * Elem.syn) | Ser_bound of SInt.syn
+
+  let ins_outs = function
+    | Ser_binding (ofs, inner_ser) ->
+        let is, os = Elem.ins_outs inner_ser in
+        (SInt.exprs_syn ofs @ is, os)
+    | Ser_bound b -> ([], SInt.exprs_syn b)
 
   let no_fix = []
-  let[@inline] lift_fix ~ofs (fix : Elem.syn) = Ser_binding (ofs, fix)
+
+  let[@inline] lift_fix ~ofs (fix : Elem.syn) =
+    Ser_binding (SInt.to_syn ofs, fix)
+
   let[@inline] lift_fixes ~ofs = List.map @@ List.map (lift_fix ~ofs)
   let of_opt = function None -> (M.empty, None) | Some l -> l
   let[@inline] add_opt k v m = M.update k (fun _ -> v) m
@@ -57,23 +62,29 @@ struct
       (M.bindings m)
 
   let pp ft t = pp' ~elem:Elem.pp ft t
+  let show = Fmt.to_to_string pp
 
   let to_syn (m, b) : syn list =
     let bindings =
       M.to_seq m
       |> Seq.concat_map (fun (k, v) ->
-          Elem.to_syn v |> List.to_seq |> Seq.map (fun v -> Ser_binding (k, v)))
+          Elem.to_syn v
+          |> List.to_seq
+          |> Seq.map (fun v -> Ser_binding (SInt.to_syn k, v)))
       |> List.of_seq
     in
-    match b with None -> bindings | Some b -> Ser_bound b :: bindings
+    match b with
+    | None -> bindings
+    | Some b -> Ser_bound (SInt.to_syn b) :: bindings
 
   let pp_syn : Format.formatter -> syn -> unit =
    fun ft ser ->
     match ser with
     | Ser_binding (k, v) ->
-        Fmt.pf ft "Binding: %a -> %a" SInt.pp k Elem.pp_syn v
-    | Ser_bound b -> Fmt.pf ft "Bound: %a" SInt.pp b
+        Fmt.pf ft "Binding: %a -> %a" SInt.pp_syn k Elem.pp_syn v
+    | Ser_bound b -> Fmt.pf ft "Bound: %a" SInt.pp_syn b
 
+  let show_syn = Fmt.to_to_string pp_syn
   let empty = M.empty
 
   (* Symbolic process that under-approximates Map.find_opt *)
@@ -167,8 +178,9 @@ struct
    *  in
    *  to_opt (m, new_b) *)
 
-  let produce (syn : syn) : unit SM.t =
-    let* st = SM.get_state () in
+  let produce (syn : syn) (st : SM.st) : SM.st Symex.Producer.t =
+    let open Symex.Producer in
+    let open Symex.Producer.Syntax in
     let m, b = of_opt st in
     let in_bounds_opt x =
       match b with
@@ -178,12 +190,15 @@ struct
     match syn with
     | Ser_bound b_ser -> (
         match b with
-        | None -> SM.set_state (to_opt (m, Some b_ser))
-        | Some _ -> SM.vanish ())
+        | None ->
+            let+ new_b = Symex.Producer.apply_subst SInt.subst b_ser in
+            to_opt (m, Some new_b)
+        | Some _ -> vanish ())
     | Ser_binding (ofs, inner_ser) ->
-        let* () = SM.assume [ in_bounds_opt ofs ] in
+        let* ofs = Symex.Producer.apply_subst SInt.subst ofs in
+        let*^ () = Symex.assume [ in_bounds_opt ofs ] in
         let*^ ofs, codom = find_opt_sym ofs m in
-        let*^ (), codom = Elem.produce inner_ser codom in
+        let+ codom = Elem.produce inner_ser codom in
         let m = add_opt ofs codom m in
-        SM.set_state (to_opt (m, b))
+        to_opt (m, b)
 end
