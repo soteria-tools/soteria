7ad210b673b0dbafc955fea7d5322f6c99871e3d
N1ark <opale.sjostedt@gmail.com>
2026-03-22 21:02:24 +0000

    reorganise interface

--- soteria-rust/lib/tree_borrows/tree_borrows_intf.ml
+++ soteria-rust/lib/tree_borrows/tree_borrows_intf.ml
@@ -1,10 +1,12 @@
 open Rustsymex
 
 module type S = sig
-  type tag
-  and access = Read | Write
+  (** {2 Tree Borrows trees (the general structure)} *)
 
-  and state =
+  type tag
+  type access = Read | Write
+
+  type state =
     | Reserved of bool
     | Unique
     | Frozen
@@ -13,40 +15,18 @@ module type S = sig
     | Disabled
     | UB
 
-  and protector = Strong | Weak
-  and t
-  and tb_state
+  type protector = Strong | Weak
+  type t
 
-  val fresh_tag : unit -> tag
-  val zero : tag
   val pp : Format.formatter -> t -> unit
   val pp_tag : Format.formatter -> tag -> unit
   val pp_state : Format.formatter -> state -> unit
+
+  (** {2 Tree Borrows state (the per-byte information)} *)
+
+  type tb_state
+
   val pp_tb_state : Format.formatter -> tb_state -> unit
-  val init : state:state -> unit -> t * tag
-  val ub_state : t
-
-  val add_child :
-    parent:tag -> ?protector:protector -> state:state -> t -> t * tag
-
-  val unprotect : tag -> t -> t
-  val strong_protector_exists : t -> bool
-  val empty_state : tb_state
-  val is_empty_state : tb_state -> bool
-  val equal_state : tb_state -> tb_state -> bool
-  val set_protector : protected:bool -> tag -> t -> tb_state -> tb_state
-
-  (** [access root accessed e state]: Update all nodes in the mapping [state]
-      for the tree rooted at [root] with an event [e], that happened at
-      [accessed]. *)
-  val access :
-    tag ->
-    access ->
-    t ->
-    tb_state ->
-    (tb_state, [> `AliasingError ], 'm) Result.t
-
-  val merge : tb_state -> tb_state -> tb_state
 
   (* Compositionality *)
 
@@ -78,4 +58,36 @@ module type S = sig
     (tb_state, 'err, serialized_state list) Rustsymex.Result.t
 
   val produce_state : serialized_state -> tb_state -> tb_state Rustsymex.t
+
+  (** {2 Operations on the structure} *)
+
+  val fresh_tag : unit -> tag
+  val zero : tag
+  val ub_state : t
+  val init : state:state -> unit -> t * tag
+
+  val add_child :
+    parent:tag -> ?protector:protector -> state:state -> t -> t * tag
+
+  val unprotect : tag -> t -> t
+  val strong_protector_exists : t -> bool
+
+  (** {2 Operations on the state} *)
+
+  val empty_state : tb_state
+  val is_empty_state : tb_state -> bool
+  val equal_state : tb_state -> tb_state -> bool
+  val set_protector : protected:bool -> tag -> t -> tb_state -> tb_state
+
+  (** [access root accessed e state]: Update all nodes in the mapping [state]
+      for the tree rooted at [root] with an event [e], that happened at
+      [accessed]. *)
+  val access :
+    tag ->
+    access ->
+    t ->
+    tb_state ->
+    (tb_state, [> `AliasingError ], 'm) Result.t
+
+  val merge : tb_state -> tb_state -> tb_state
 end
