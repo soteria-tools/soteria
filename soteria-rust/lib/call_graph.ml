188b0a8bb749bde86fd5049ba0707b5c640169b2
N1ark <opale.sjostedt@gmail.com>
2026-04-21 16:55:07 +0100

    Clean up this ad-hoc mess

--- soteria-rust/lib/call_graph.ml
+++ soteria-rust/lib/call_graph.ml
@@ -8,48 +8,41 @@ open Charon
 
 (** Node type: a fully-qualified function name rendered as a string. *)
 module FunNode = struct
-  type t = string
+  type t = { short_name : string; long_name : string } [@@deriving eq, hash]
 
-  let equal = String.equal
-  let hash = Hashtbl.hash
-  let pp = Fmt.string
-  let long_name s = s
+  let pp ft { long_name; _ } = Format.pp_print_string ft long_name
+  let long_name s = s.long_name
+  let short_name s = s.short_name
 
-  (** The short label is the last component of the dotted name. *)
-  let short_name s =
-    let end_ = String.length s - 1 in
-    let end_ =
-      if String.get s (String.length s - 1) = '>' then
-        (* Handle case of poly name, e.g. `f::<T>` *)
-        match String.rindex_opt s ':' with
-        | Some i -> i - 2
-        | None -> Fmt.failwith "Unexpected function name format: %s" s
-      else end_
+  (** The "short name" is the last identifier, if there is one (should always be
+      the case), fallbacks to the full name *)
+  let short_name_of_name n =
+    let rec aux : Types.name -> Types.name option = function
+      | [] -> None
+      | (PeIdent _ as short) :: _ -> Some [ short ]
+      | _ :: rest -> aux rest
     in
-    match String.rindex_from_opt s end_ ':' with
-    | Some i -> String.sub s (i + 1) (String.length s - i - 1)
-    | None -> s
+    match aux (List.rev n) with None -> n | Some short -> short
+
+  let of_name n =
+    let name_str n = (Fmt.to_to_string Crate.pp_name) n in
+    let long_name = name_str n in
+    let short_name = name_str (short_name_of_name n) in
+    { short_name; long_name }
 end
 
 module G = Graph.Make_with_dot (FunNode)
 
-(* ------------------------------------------------------------------ *)
-(* Global state – NOT concurrent-safe.                                *)
-(* Will be reused across tests.                                       *)
-(* ------------------------------------------------------------------ *)
-
+(** The global call graph. Nodes are fully-qualified function names as strings.
+    This is not concurrent-safe. *)
 let graph : G.t = G.with_node_capacity 64
-
-(* ------------------------------------------------------------------ *)
-(* Public API                                                           *)
-(* ------------------------------------------------------------------ *)
 
 (** Adds an edge to the global callgraph. Always needs to be called within a
     crate context! *)
 let add_edge (from : Types.name option) (to_ : Types.name option) =
-  let name_str n = (Fmt.to_to_string Crate.pp_name) n in
   match (from, to_) with
-  | Some from, Some to_ -> G.add_edge graph (name_str from) (name_str to_)
+  | Some from, Some to_ ->
+      G.add_edge graph (FunNode.of_name from) (FunNode.of_name to_)
   | _ -> ()
 
 (** Dump the call graph as a DOT file at [path]. *)
