143ee2d162d0efab864162ad5ab1b2bf31a8e220
Sacha Ayoun <sachaayoun@gmail.com>
2026-04-24 11:23:14 +0100

    The "accept commit" thing really should have a formatting option
    Signed-off-by: Sacha Ayoun <sachaayoun@gmail.com>

--- soteria/lib/symex/symex.ml
+++ soteria/lib/symex/symex.ml
@@ -1042,8 +1042,7 @@ module Base_extension (Core : Core) = struct
      fun subst ->
       match Value.Expr.Subst.learn subst expr v with
       | None ->
-          [%l.debug
-              "Couldn't learn %a := %a" Value.Expr.pp expr Value.ppa v];
+          [%l.debug "Couldn't learn %a := %a" Value.Expr.pp expr Value.ppa v];
           lfail (Value.of_bool false) subst
       | Some subst ->
           let v', subst =
