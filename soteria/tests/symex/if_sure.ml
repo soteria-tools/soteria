15f3b1ba52f2e52ae270549e142e4f7d9bef42a7
N1ark <opale.sjostedt@gmail.com>
2026-05-11 17:38:31 +0100

    clean tests

--- soteria/tests/symex/if_sure.ml
+++ soteria/tests/symex/if_sure.ml
@@ -19,13 +19,13 @@ let if_guaranteed =
   let* () = assume [ x >@ one ] in
   if%sure x >@ zero then return 42 else return (-1)
 
-let if_sure_runs_then_with_consistent_pc =
+let if_guaranteed_no_simp =
   let* x = nondet t_int in
   let* y = nondet t_int in
   let* () = assume [ (x *@ x) +@ (y *@ y) ==@ zero ] in
   if%sure x ==@ zero then return 42 else return (-1)
 
-let if_sure_else_preserves_both_arms =
+let if_sure_not_guaranteed_branching =
   let* x = nondet t_int in
   if%sure x ==@ zero then return 0
   else if%sat x ==@ zero then return 1
@@ -37,35 +37,25 @@ let if_sure_else_preserves_both_arms =
 
 let get_results process = List.map fst (run ~mode:OX process)
 
+let check_results ?(sorted = false) process expected =
+  let results = get_results process in
+  let results = if sorted then List.sort Stdlib.compare results else results in
+  Alcotest.(check (list int)) "result" expected results
+
 (* ============================================================================
    Tests
    ============================================================================ *)
 
-let if_true_produces_42 () =
-  let results = get_results if_true in
-  Alcotest.(check (list int)) "result" [ 42 ] results
-
-let if_false_produces_42 () =
-  let results = get_results if_false in
-  Alcotest.(check (list int)) "result" [ 42 ] results
-
-let if_maybe_produces_42 () =
-  let results = get_results if_maybe in
-  Alcotest.(check (list int)) "result" [ 42 ] results
-
-let if_guaranteed_produces_42 () =
-  let results = get_results if_guaranteed in
-  Alcotest.(check (list int)) "result" [ 42 ] results
+let if_true_produces_42 () = check_results if_true [ 42 ]
+let if_false_produces_42 () = check_results if_false [ 42 ]
+let if_maybe_produces_42 () = check_results if_maybe [ 42 ]
+let if_guaranteed_produces_42 () = check_results if_guaranteed [ 42 ]
 
 let if_sure_runs_then_with_consistent_pc_test () =
-  let results = get_results if_sure_runs_then_with_consistent_pc in
-  Alcotest.(check (list int)) "result" [ 42 ] results
+  check_results if_guaranteed_no_simp [ 42 ]
 
 let if_sure_else_preserves_both_arms_test () =
-  let results =
-    get_results if_sure_else_preserves_both_arms |> List.sort Stdlib.compare
-  in
-  Alcotest.(check (list int)) "result" [ 1; 2 ] results
+  check_results ~sorted:true if_sure_not_guaranteed_branching [ 1; 2 ]
 
 (* ============================================================================
    Test Runner
