e4b965f1cd677a7fbef0f66132f01af2fbc5fb05
Sacha Ayoun <sachaayoun@gmail.com>
2026-05-17 22:26:54 +0100

    sysconf implem
    Signed-off-by: Sacha Ayoun <sachaayoun@gmail.com>

--- soteria-rust/lib/builtins/extern/libc.ml
+++ soteria-rust/lib/builtins/extern/libc.ml
@@ -2,9 +2,9 @@ open Rust_val
 open Typed.Syntax
 open Typed.Infix
 
-type fn = Exit
+type fn = Exit | Sysconf
 
-let fn_pats = [ ("exit", Exit) ]
+let fn_pats = [ ("exit", Exit); ("sysconf", Sysconf) ]
 
 module M (StateM : State.StateM.S) = struct
   open StateM
@@ -16,5 +16,15 @@ module M (StateM : State.StateM.S) = struct
         if%sat code ==@ U32.(0s) then error `OkExit else error (`Exit code)
     | _ -> failwith "exit: invalid arguments"
 
-  let[@inline] fn_to_stub = function Exit -> exit
+  let sysconf args =
+    match args with
+    | [ Int _ ] ->
+        (* https://man7.org/linux/man-pages/man3/sysconf.3.html
+         * It is basically ok to always return the i64 `-1` saying "I don't know"
+         *)
+        let ret = Typed.BitVec.u64 Z.minus_one in
+        ok (Int ret)
+    | _ -> failwith "sysconf: invalid arguments"
+
+  let[@inline] fn_to_stub = function Exit -> exit | Sysconf -> sysconf
 end
