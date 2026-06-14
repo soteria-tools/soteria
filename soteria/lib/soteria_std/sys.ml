f00c0425d37168b536dec7a83a2b0f0dee42a8af
Sacha Ayoun <sachaayoun@gmail.com>
2026-06-14 22:39:05 +0100

    add tests for subprocess
    Signed-off-by: Sacha Ayoun <sachaayoun@gmail.com>

--- soteria/lib/soteria_std/sys.ml
+++ soteria/lib/soteria_std/sys.ml
@@ -6,3 +6,8 @@ let with_working_dir dir f =
   let old_dir = getcwd () in
   chdir dir;
   Fun.protect ~finally:(fun () -> chdir old_dir) f
+
+(* When used in Unix.kill, the "null signal" delivers nothing but still runs
+   kill(2)'s error checks, so it reports whether a pid still names a process --
+   the POSIX liveness probe. *)
+let signull = 0
