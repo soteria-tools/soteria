c5b19aab77ada3130a701699634c6edd1a33a911
Sacha Ayoun <sachaayoun@gmail.com>
2026-06-15 11:55:06 +0100

    oops
    Co-authored-by: opale <opale.sjostedt@gmail.com>

--- soteria/lib/soteria_std/sys.ml
+++ soteria/lib/soteria_std/sys.ml
@@ -7,7 +7,7 @@ let with_working_dir dir f =
   chdir dir;
   Fun.protect ~finally:(fun () -> chdir old_dir) f
 
-(* When used in Unix.kill, the "null signal" delivers nothing but still runs
+(** When used in Unix.kill, the "null signal" delivers nothing but still runs
    kill(2)'s error checks, so it reports whether a pid still names a process --
    the POSIX liveness probe. *)
 let signull = 0
