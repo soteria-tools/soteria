ba70143c37306b7664cea07a575647627077b2d4
Sacha Ayoun <sachaayoun@gmail.com>
2026-06-29 16:08:36 +0100

    add lazy map
    Signed-off-by: Sacha Ayoun <sachaayoun@gmail.com>

--- soteria/lib/sym_states/pmap.mli
+++ soteria/lib/sym_states/pmap.mli
@@ -28,6 +28,18 @@ module Direct_access_patricia_tree
     (Codom : Base.M(Symex).S) :
   S(Symex)(Key).S with type codom := Codom.t and type codom_syn := Codom.syn
 
+module Lazy
+    (Symex : Symex.Base)
+    (Key : Key(Symex).S)
+    (Codom : Base.M(Symex).S) :
+  S(Symex)(Key).S with type codom := Codom.t and type codom_syn := Codom.syn
+
+module Lazy_patricia_tree
+    (Symex : Symex.Base)
+    (Key : Key(Symex).S_patricia_tree)
+    (Codom : Base.M(Symex).S) :
+  S(Symex)(Key).S with type codom := Codom.t and type codom_syn := Codom.syn
+
 module Concrete
     (Symex : Symex.Base)
     (Key : Soteria_std.Ordered_type.S)
