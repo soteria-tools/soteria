fb81a9634bbbd13c25b7166b293d0a3bc0f97a89
N1ark <opale.sjostedt@gmail.com>
2026-07-23 09:42:25 +0100

    Use `ppx_matches`

--- soteria-rust/lib/state/tree_state.ml
+++ soteria-rust/lib/state/tree_state.ml
@@ -302,7 +302,7 @@ module Make (Borrows : Tree_borrows.T) = struct
   let pp_pretty ~ignore_freed ft { heap; _ } =
     let (ignore : 'a * Freeable_block_with_meta.t -> bool) =
       if ignore_freed then [%matches? _, { node = Freed; _ }]
-      else fun _ -> false
+      else Fun.const false
     in
     match heap with
     | None -> Fmt.pf ft "Empty State"
