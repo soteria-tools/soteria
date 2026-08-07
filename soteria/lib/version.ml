d4fffe217199849a1f71a38c5e695078e690671d
opale <opale.sjostedt@gmail.com>
2026-08-07 13:21:52 +0100

    Release v0.2.2 (#473)

--- soteria/lib/version.ml
+++ soteria/lib/version.ml
@@ -1,5 +1,5 @@
 (* Kept in sync with scripts/versions.json by scripts/versionsync.py; do not
    edit the version string by hand. *)
 
-(* [versionsync: SOTERIA_VERSION=0.2.1] *)
-let version = "0.2.1"
+(* [versionsync: SOTERIA_VERSION=0.2.2] *)
+let version = "0.2.2"
