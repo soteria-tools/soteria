acd52cb732792a01914bb802b90d358a2cebb2db
N1ark <opale.sjostedt@gmail.com>
2026-08-13 20:24:16 +0200

    lol. lmao even.

--- soteria/lib/bv_values/svalue.ml
+++ soteria/lib/bv_values/svalue.ml
@@ -1214,9 +1214,9 @@ module Make (V : Value_ext) () = struct
     (* Index [n-1] holds the cached value for bitwidth [n]; we skip [n=0]
        because [mk] asserts [n > 0]. *)
     let zero_cache : t Array.t = Array.init 256 (fun n -> mk (n + 1) Z.zero)
-    let[@inline] zero n = if n <= 256 then zero_cache.(n - 1) else mk n Z.one
+    let[@inline] zero n = if n <= 256 then zero_cache.(n - 1) else mk n Z.zero
     let one_cache : t Array.t = Array.init 256 (fun n -> mk (n + 1) Z.one)
-    let[@inline] one n = if n <= 256 then one_cache.(n - 1) else mk n Z.zero
+    let[@inline] one n = if n <= 256 then one_cache.(n - 1) else mk n Z.one
 
     (** [bv_to_z signed bits z] parses a BitVector [z], for a given bitwidth
         [bits], with [signed], into an integer. *)
