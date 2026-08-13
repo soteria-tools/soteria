f746434136f4a20d2e66ede02362696fcc2613a5
N1ark <opale.sjostedt@gmail.com>
2026-08-13 22:59:08 +0200

    rust: stub miri_track_alloc and miri_write_to_stderr
    Both are diagnostic-only, like the Miri externs already stubbed here.

--- soteria-rust/lib/builtins/extern/miri.ml
+++ soteria-rust/lib/builtins/extern/miri.ml
@@ -15,7 +15,9 @@ let fn_pats =
     ("miri_pointer_name", Nop);
     ("miri_print_borrow_state", Nop);
     ("miri_run_provenance_gc", Nop);
+    ("miri_track_alloc", Nop);
     ("miri_write_to_stdout", Nop);
+    ("miri_write_to_stderr", Nop);
     ("miri_alloc", Alloc);
     ("miri_dealloc", Dealloc);
     ("miri_promise_symbolic_alignment", PromiseAlignment);
