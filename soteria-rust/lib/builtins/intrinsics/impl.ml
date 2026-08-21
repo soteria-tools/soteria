5c8c0a1ea316bb04059a5cbe838692788f230818
N1ark <opale.sjostedt@gmail.com>
2026-08-21 22:42:58 +0200

    review: remove typo ^^

--- soteria-rust/lib/builtins/intrinsics/impl.ml
+++ soteria-rust/lib/builtins/intrinsics/impl.ml
@@ -843,7 +843,6 @@ module M (StateM : State.StateM.S) : Intf.M(StateM).Impl = struct
   let prefetch_read_instruction ~t:_ ~locality:_ ~data:_ = ok ()
   let prefetch_write_data ~t:_ ~locality:_ ~data:_ = ok ()
   let prefetch_write_instruction ~t:_ ~locality:_ ~data:_ = ok ()
-  let ptr_guaranteed_cmp ~t:_ ~ptr ~other = Core.eval_ptr_binop Eq ptr other
 
   let ptr_guaranteed_cmp ~t ~ptr ~other =
     Core.eval_ptr_binop ~pointee:t Eq ptr other
